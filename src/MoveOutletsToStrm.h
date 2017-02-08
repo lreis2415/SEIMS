bcf4e366dd402ae27620048f78231c1ed400caf1
crazyzlj <crazyzlj@gmail.com>
2017-02-08 17:40:15 +0800

    Squashed 'seims/src/taudem/' content from commit 96dd4b4
    git-subtree-dir: seims/src/taudem
    git-subtree-split: 96dd4b423be094b636b9d5f0e4a9ffe161f08388

---
+++ src/MoveOutletsToStrm.h
@@ -0,0 +1,4 @@
+#include "linearpart.h"
+
+
+int outletstosrc(char *pfile, char *srcfile, char *outletsdatasrc, char *outletslayer,int uselyrname,int lyrno, char *outletmoveddatasrc,char *outletmovedlayer, int maxdist);
