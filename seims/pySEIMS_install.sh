3dd3d6402ad389bbb303376c2a29c373492d50a5
crazyzlj <crazyzlj@gmail.com>
2017-02-15 23:32:59 +0800

    update

---
+++ seims/pySEIMS_install.sh
@@ -0,0 +1,15 @@
+#
+#    This file is aimed for Packages checking and installing for SEIMS on Linu/Unix platform.
+#
+#    SEIMS is distributed for Research and/or Education only,
+#    any commercial purpose will be FORBIDDEN.
+#    SEIMS is an open-source project,
+#    but WITHOUT ANY WARRANTY; WITHOUT even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    See the GNU General Public License for more details.
+#
+
+
+cd pygeoc
+python setup.py install
