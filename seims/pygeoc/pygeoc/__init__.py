87c65614f4fca919d4d6ffd8eae8e29f2ee93815
crazyzlj <crazyzlj@gmail.com>
2017-02-16 15:11:51 +0800

    更改pygeoc基本信息输入

--- seims/pygeoc/pygeoc/__init__.py
+++ seims/pygeoc/pygeoc/__init__.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 #coding=utf-8
-def main():
-    """Entry point for PyGeoC"""
-    from utils import *
+# def main():
+#     """Entry point for PyGeoC"""
+#     from utils import *
+__author__ = 'LiangJun Zhu'
+__email__ = 'zlj@lreis.ac.cn'
+__version__ = '0.1.1'
+__url__ = 'https://github.com/lreis2415/PyGeoC'
