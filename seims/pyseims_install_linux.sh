75a83ead68e672619b0702ec567edd8536c599f8
crazyzlj <crazyzlj@gmail.com>
2017-02-16 15:10:24 +0800

    增加python依赖库的自动安装脚本（必须首先安装好pip）

--- seims/pyseims_install_linux.sh
+++ seims/pyseims_install_linux.sh
@@ -10,4 +10,15 @@
 #
 #    See the GNU General Public License for more details.
 #
+cd $PWD
+cd pygeoc
+rm -r dist
+# Compile and install PyGeoC through pip
+pip install tox
+python setup.py bdist_wheel --python-tag py2
+cd dist
+for i in `find . -name *.whl`; do pip install $i --upgrade; done
 
+cd ../..
+# Install all dependent packages requied by SEIMS
+for i in `find . -name requirements.txt`; do pip install -r $i; done
