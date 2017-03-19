d5219ea2286a21868cf315e8f0f392c15cdd025e
crazyzlj <crazyzlj@gmail.com>
2017-03-19 16:36:00 +0800

    subtree command: add, pull, and push

---
+++ doc/subtree/push_subtree.sh
@@ -0,0 +1,14 @@
+#git subtree push --prefix=doc/wiki https://github.com/lreis2415/SEIMS2017.wiki.git master
+#git subtree push --prefix=seims/pygeoc git@github.com:lreis2415/PyGeoC.git master
+#git subtree push --prefix=seims/src/taudem git@github.com:lreis2415/TauDEM.git master
+#git subtree push --prefix=seims/src/metis git@github.com:lreis2415/metis.git master
+#git subtree push --prefix=seims/src/commonlibs/UtilsClass git@github.com:lreis2415/UtilsClass.git master
+#git subtree push --prefix=seims/src/commonlibs/MongoUtilClass git@github.com:lreis2415/MongoUtilClass.git master
+#git subtree push --prefix=seims/src/commonlibs/RasterClass git@github.com:lreis2415/RasterClass.git master
+git subtree push --prefix=doc/wiki wiki master
+git subtree push --prefix=seims/pygeoc pygeoc master
+git subtree push --prefix=seims/src/taudem taudem master
+git subtree push --prefix=seims/src/metis metis master
+git subtree push --prefix=seims/src/commonlibs/UtilsClass utilsclass master
+git subtree push --prefix=seims/src/commonlibs/MongoUtilClass mongoutilclass master
+git subtree push --prefix=seims/src/commonlibs/RasterClass rasterclass master
