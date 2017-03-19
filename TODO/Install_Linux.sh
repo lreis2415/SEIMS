a9495749e88f4c01073c6a0f9d9d6d3acd7b7eac
crazyzlj <crazyzlj@gmail.com>
2017-03-19 16:35:07 +0800

    update todo

--- TODO/Install_Linux.sh
+++
@@ -1,9 +0,0 @@
-# This is the automated installation script for Linux, e.g. CentOS.
-# Created by Liang-Jun Zhu
-# Date from 2016-12-15
-# The basic procedure is as follows:
-#    1. Set the basic environment path for essential requi#ents.
-#    2. Check if the required environment is satisfied, GDAL, MongoDB, etc..
-#    3. Compile preprocess programe and main SEIMS.
-#    4. Install SEIMS with workflow scripts.
-#    5. Enjoy SEIMS!
--- TODO/Install_Windows.bat
+++
@@ -1,9 +0,0 @@
-REM This is the automated installation script for Windows.
-REM Created by Liang-Jun Zhu
-REM Date from 2016-12-15
-REM The basic procedure is as follows:
-REM    1. Set the basic environment path for essential requirements.
-REM    2. Check if the required environment is satisfied, GDAL, MongoDB, etc..
-REM    3. Compile preprocess programe and main SEIMS.
-REM    4. Install SEIMS with workflow scripts.
-REM    5. Enjoy SEIMS!
