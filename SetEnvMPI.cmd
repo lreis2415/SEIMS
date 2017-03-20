95504f931bd99b3e4e5e32a5cdbdf8669c5f894d
crazyzlj <crazyzlj@gmail.com>
2017-03-20 16:23:04 +0800

    debug ci

--- SetEnvMPI.cmd
+++
@@ -1,4 +0,0 @@
-SET MSMPI_BIN=C:\Program Files\Microsoft MPI\Bin\
-SET MSMPI_INC=C:\Program Files (x86)\Microsoft SDKs\MPI\Include\
-SET MSMPI_LIB32=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x86\
-SET MSMPI_LIB64=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64\
