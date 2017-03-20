5b05c950a97d52fa016365ee10d34f59ee235e38
crazyzlj <crazyzlj@gmail.com>
2017-03-20 12:39:19 +0800

    debug ci

---
+++ SetEnvMPI.cmd
@@ -0,0 +1,4 @@
+SET MSMPI_BIN=C:\Program Files\Microsoft MPI\Bin\
+SET MSMPI_INC=C:\Program Files (x86)\Microsoft SDKs\MPI\Include\
+SET MSMPI_LIB32=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x86\
+SET MSMPI_LIB64=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64\
--- doc/ci/travis/osx_xcode/install.sh
+++ doc/ci/travis/osx_xcode/install.sh
@@ -5,7 +5,6 @@ cd seims
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
 make -j4
 sudo make install
 cd ..
