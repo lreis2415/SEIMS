f2146a7002ad70ea964e93ebb16b6942cf2cfff5
crazyzlj <crazyzlj@gmail.com>
2017-03-21 12:55:43 +0800

    bug fixed when compiled by MSVC 2013 or later

--- seims/src/metis/GKlib/gk_arch.h
+++ seims/src/metis/GKlib/gk_arch.h
@@ -58,13 +58,15 @@ typedef ptrdiff_t ssize_t;
 #endif
 
 #ifdef __MSC__
+// Add version check to solve compile error on MSVC 2013+
+//   refers to https://github.com/INMOST-DEV/INMOST/issues/14
+#if _MSC_VER < 1700
 /* MSC does not have rint() function */
-#define rint(x) ((int)((x)+0.5))  
-
+#define rint(x) ((int)((x)+0.5))
+#endif
 /* MSC does not have INFINITY defined */
 #ifndef INFINITY
 #define INFINITY FLT_MAX
 #endif
 #endif
-
 #endif
