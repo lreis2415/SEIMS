71eff9ab36b79ca42c077f6daa2bc60d642ba5a7
crazyzlj <crazyzlj@gmail.com>
2017-03-21 12:52:47 +0800

    update subtree commands

--- doc/subtree/add_subtree.sh
+++ doc/subtree/add_subtree.sh
@@ -11,9 +11,9 @@ git subtree add --prefix=seims/src/taudem taudem master --squash
 git remote add -f metis git@github.com:lreis2415/metis.git master
 git subtree add --prefix=seims/src/metis metis master --squash
 # commonlibs
-git remote add -f utilsclass git@github.com:lreis2415/UtilsClass.git
-git remote add -f mongoutilclass git@github.com:lreis2415/MongoUtilClass.git
-git remote add -f rasterclass git@github.com:lreis2415/RasterClass.git
+git remote add -f utilsclass git@github.com:lreis2415/UtilsClass.git master
+git remote add -f mongoutilclass git@github.com:lreis2415/MongoUtilClass.git master
+git remote add -f rasterclass git@github.com:lreis2415/RasterClass.git master
 git subtree add --prefix=seims/src/commonlibs/UtilsClass utilsclass master --squash
 git subtree add --prefix=seims/src/commonlibs/MongoUtilClass mongoutilclass master --squash
 git subtree add --prefix=seims/src/commonlibs/RasterClass rasterclass master --squash
