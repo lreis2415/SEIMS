572afd64e4c68f6e162f68bb62610b3cea437004
crazyzlj <crazyzlj@gmail.com>
2017-03-21 12:48:34 +0800

    Squashed 'seims/src/taudem/' content from commit 9c84d09
    git-subtree-dir: seims/src/taudem
    git-subtree-split: 9c84d09d18b1469ca7d1a6eb827d2fce50267f54

---
+++ src/areadinf.h
@@ -0,0 +1,11 @@
+
+int area(char *angfile,
+         char *scafile,
+         char *datasrc,
+         char *lyrname,
+         int uselyrname,
+         int lyrno,
+         char *wfile,
+         int useOutlets,
+         int usew,
+         int contcheck);
