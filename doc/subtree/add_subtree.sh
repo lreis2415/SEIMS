dd9bd0544d8623c837f9d96b3b021c7e48eb7b10
crazyzlj <crazyzlj@gmail.com>
2017-03-21 12:51:45 +0800

    readd metis as subtree

--- doc/subtree/add_subtree.sh
+++ doc/subtree/add_subtree.sh
@@ -2,13 +2,13 @@
 git remote add -f wiki https://github.com/lreis2415/SEIMS2017.wiki.git
 git subtree add --prefix=doc/wiki wiki master --squash
 # pygeoc
-git remote add -f pygeoc git@github.com:lreis2415/PyGeoC.git
+git remote add -f pygeoc git@github.com:lreis2415/PyGeoC.git master
 git subtree add --prefix=seims/pygeoc pygeoc master --squash
 # taudem
-git remote add -f taudem git@github.com:lreis2415/TauDEM.git
+git remote add -f taudem git@github.com:lreis2415/TauDEM.git master
 git subtree add --prefix=seims/src/taudem taudem master --squash
 # metis
-git remote add -f metis git@github.com:lreis2415/metis.git
+git remote add -f metis git@github.com:lreis2415/metis.git master
 git subtree add --prefix=seims/src/metis metis master --squash
 # commonlibs
 git remote add -f utilsclass git@github.com:lreis2415/UtilsClass.git
