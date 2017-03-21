8e55afc7cae6e42c7516d9520f8592bd266bf1fc
crazyzlj <crazyzlj@gmail.com>
2017-03-21 12:59:16 +0800

    readd commonlibs as subtree

--- seims/src/commonlibs/UtilsClass/utilities.h
+++ seims/src/commonlibs/UtilsClass/utilities.h
@@ -30,48 +30,49 @@
 #define SplitString                utilsString::SplitString
 #define SplitStringForValues    utilsString::SplitStringForValues
 #define SplitStringForInt        utilsString::SplitStringForInt
-#define SplitStringForFloat        utilsString::SplitStringForFloat
+#define SplitStringForFloat       utilsString::SplitStringForFloat
 #define ValueToString            utilsString::ValueToString
 
 /// Class utilsArray
-#define Initialize1DArray        utilsArray::Initialize1DArray
-#define Initialize2DArray        utilsArray::Initialize2DArray
+#define Initialize1DArray         utilsArray::Initialize1DArray
+#define Initialize2DArray         utilsArray::Initialize2DArray
 #define Release1DArray            utilsArray::Release1DArray
 #define Release2DArray            utilsArray::Release2DArray
 #define Output1DArrayToTxtFile    utilsArray::Output1DArrayToTxtFile
 #define Output2DArrayToTxtFile    utilsArray::Output2DArrayToTxtFile
 #define Read1DArrayFromTxtFile    utilsArray::Read1DArrayFromTxtFile
 #define Read2DArrayFromTxtFile    utilsArray::Read2DArrayFromTxtFile
-#define Read2DArrayFromString    utilsArray::Read2DArrayFromString
-#define ValueInVector            utilsArray::ValueInVector
-#define RemoveValueInVector        utilsArray::RemoveValueInVector
+#define Read2DArrayFromString     utilsArray::Read2DArrayFromString
+#define ValueInVector             utilsArray::ValueInVector
+#define RemoveValueInVector       utilsArray::RemoveValueInVector
 
 /// Class utilsMath
-#define FloatEqual                utilsMath::FloatEqual
-#define Expo                    utilsMath::Expo
-#define Power                    utilsMath::Power
+#define FloatEqual                 utilsMath::FloatEqual
+#define Expo                       utilsMath::Expo
+#define Power                      utilsMath::Power
 #define Max                        utilsMath::Max
 #define Sum                        utilsMath::Sum
 #define basicStatistics            utilsMath::basicStatistics
 
 /// Class utilsFileIO
 #ifndef windows
-#define copyfile_unix            utilsFileIO::copyfile_unix
+#define copyfile_unix             utilsFileIO::copyfile_unix
 #endif
 #define GetAppPath                utilsFileIO::GetAppPath
-#define GetCoreFileName            utilsFileIO::GetCoreFileName
-#define GetSuffix                utilsFileIO::GetSuffix
-#define ReplaceSuffix            utilsFileIO::ReplaceSuffix
-#define GetPathFromFullName        utilsFileIO::GetPathFromFullName
+#define GetCoreFileName           utilsFileIO::GetCoreFileName
+#define GetSuffix                 utilsFileIO::GetSuffix
+#define ReplaceSuffix             utilsFileIO::ReplaceSuffix
+#define GetPathFromFullName       utilsFileIO::GetPathFromFullName
 #define FileExists                utilsFileIO::FileExists
 #define PathExists                utilsFileIO::PathExists
-#define DeleteExistedFile        utilsFileIO::DeleteExistedFile
-#define FindFiles                utilsFileIO::FindFiles
+#define DeleteExistedFile         utilsFileIO::DeleteExistedFile
+#define FindFiles                 utilsFileIO::FindFiles
 
 /// Class utils
 #define isIPAddress                utils::isIPAddress
 #define Log                        utils::Log
-#define GetAvailableThreadNum    utils::GetAvailableThreadNum
-#define SetDefaultOpenMPThread    utils::SetDefaultOpenMPThread
-#define StatusMessage            utils::StatusMessage
+#define GetAvailableThreadNum      utils::GetAvailableThreadNum
+#define SetDefaultOpenMPThread     utils::SetDefaultOpenMPThread
+#define SetOpenMPThread     utils::SetOpenMPThread
+#define StatusMessage              utils::StatusMessage
 #endif
