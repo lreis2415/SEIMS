cbfed33743cda43605ec04af0ae2560b080a8a0d
crazyzlj <crazyzlj@gmail.com>
2017-04-27 19:48:05 +0800

    initial the DataCenter class

---
+++ seims/src/seims_main/base/data/DataCenter.cpp
@@ -0,0 +1,19 @@
+#include "DataCenter.h"
+
+/******* DataCenter ********/
+DataCenter::DataCenter() {
+
+}
+
+DataCenter::~DataCenter() {
+
+}
+
+/******* DataCenterMongoDB ********/
+DataCenterMongoDB::DataCenterMongoDB() {
+
+}
+
+DataCenterMongoDB::~DataCenterMongoDB() {
+
+}
