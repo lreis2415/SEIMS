85e6ea9b86890998ef821cb4bbfcbc9b68ba6109
crazyzlj <crazyzlj@gmail.com>
2017-07-05 20:08:49 +0800

    update copyright header

--- seims/scenario_analysis/__init__.py
+++ seims/scenario_analysis/__init__.py
@@ -1,16 +1,26 @@
-'''
-    This file is part of scenario_analysis of SEIMS.
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ pySEIMS
+                             Python APIs for SEIMS
+ Scenario Analysis by NSGA-II algorithm of DEAP
+                              -------------------
+        author               : Liangjun Zhu, Junzhi Liu
+        copyright            : (C) 2017 by Lreis, IGSNRR, CAS
+        email                : zlj@lreis.ac.cn
+ ******************************************************************************
+ *                                                                            *
+ *   SEIMS is distributed for Research and/or Education only, any commercial  *
+ *   purpose will be FORBIDDEN. SEIMS is an open-source project, but without  *
+ *   ANY WARRANTY, WITHOUT even the implied warranty of MERCHANTABILITY or    *
+ *   FITNESS for A PARTICULAR PURPOSE.                                        *
+ *   See the GNU General Public License for more details.                     *
+ *                                                                            *
+ ******************************************************************************/
+"""
 
-    SEIMS is distributed for Research and/or Education only,
-    any commercial purpose will be FORBIDDEN.
-    SEIMS is an open-source project,
-    but WITHOUT ANY WARRANTY; WITHOUT even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-    See the GNU General Public License for more details.
-'''
-__author__ = "SEIMS Team"
+__author__ = "Huiran Gao"
 __description__ = "Implementation of the Scenario Analysis by NSGA-II algorithm of DEAP"
-__contributors__ = "Huiran Gao"
 __version__ = "1.0"
 __revision__ = "1.0.0"
