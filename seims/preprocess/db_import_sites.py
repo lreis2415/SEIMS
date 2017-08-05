9bde164e3f01a1e1bf8b8a858fa9a833d7ef9004
crazyzlj <crazyzlj@gmail.com>
2017-08-05 22:27:06 +0800

    add timesystem and timezone preprocessor at the first line of data file

--- seims/preprocess/db_import_sites.py
+++ seims/preprocess/db_import_sites.py
@@ -8,7 +8,6 @@
 """
 from osgeo.ogr import Open as ogr_Open
 from pymongo import ASCENDING
-from shapely.wkt import loads as shapely_loads
 
 from seims.preprocess.text import StationFields, DBTableNames, VariableDesc, DataType, FieldNames
 from seims.preprocess.utility import read_data_items_from_txt, DEFAULT_NODATA
@@ -107,10 +106,18 @@ class ImportHydroClimateSites(object):
     @staticmethod
     def ogrwkt2shapely(input_shape, id_field):
         """Return shape objects list and ids list"""
+        # CAUTION, IMPORTANT
+        # Because shapely is dependent on sqlite, and the version is not consistent
+        #    with GDAL executable (e.g., located in C:\GDAL_x64\bin), thus the shapely
+        #    must be locally imported here.
+        from shapely.wkt import loads as shapely_loads
         shapely_objects = []
         id_list = []
         # print input_shape
         shp = ogr_Open(input_shape)
+        if shp is None:
+            raise RuntimeError("The input ESRI Shapefile: %s is not existed or has "
+                               "no read permission!" % input_shape)
         lyr = shp.GetLayer()
         for n in range(0, lyr.GetFeatureCount()):
             feat = lyr.GetFeature(n)
