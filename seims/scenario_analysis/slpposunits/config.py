57f54590353b5b767e53591e6b5142a882ba4916
crazyzlj <crazyzlj@gmail.com>
2017-08-18 21:35:05 +0800

    add specific application of SA

---
+++ seims/scenario_analysis/slpposunits/config.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Configuration BMPs optimization based on slope position units.
+    @author   : Huiran Gao, Liangjun Zhu
+    @changelog: 16-12-30  hr - initial implementation.\n
+                17-08-18  lj - reorganize as basic class.\n
+"""
---
+++ seims/scenario_analysis/slpposunits/scenario.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Scenario for optimizing BMPs based on slope position units.
+    @author   : Huiran Gao, Liangjun Zhu
+    @changelog: 16-10-29  hr - initial implementation.\n
+                17-08-18  lj - redesign and rewrite.\n
+"""
