318eb72acb79f65477a2c7ad05b954072ee8dff1
crazyzlj <crazyzlj@gmail.com>
2017-08-18 21:35:05 +0800

    add specific application of SA

---
+++ seims/scenario_analysis/slpposunits/userdef.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""User defined operation for optimizing BMPs based on slope position units.
+    @author   : Huiran Gao, Liangjun Zhu
+    @changelog: 16-11-08  hr - initial implementation.\n
+                17-08-18  lj - reorganization.\n
+"""
