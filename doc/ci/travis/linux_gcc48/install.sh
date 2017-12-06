a748427b2de14dcb7c83fcfd1f8b8159d7847bcf
crazyzlj <crazyzlj@gmail.com>
2017-12-06 21:01:30 +0800

    Squashed 'seims/src/commonlibs/UtilsClass/' changes from 3e1906914..eeed8a852
    eeed8a852 update
    352c0029b add DeleteDirectory func, and other update
    3c1966023 update
    e235c9972 update
    7271a942c update ci script to run unittest
    ed416743d add unittest of utilsFileIO class
    0e2cab727 add test data dir
    0a2e54cc9 update
    4980994bd code style
    04137160c update
    5bb0ee649 test
    c4db81592 test
    bb1feb5a7 update
    744281f66 Merge commit 'c1614379115880e913510d60bcf0f38dbbf2799d'
    c16143791 Squashed 'cmake/' changes from bb11969..03d5650
    54901bb41 code reorganization
    b15633020 update
    a3117a950 update
    bc0f71b19 debug coverage
    5f73f99ea debug unittest
    1dabb925d add unittest based gtest
    0cdd4d7ea Squashed 'cmake/' content from commit bb11969
    dff0369f9 Merge commit '0cdd4d7eabbb8ae3caafb1f6790fdd2ec02bca86' as 'cmake'
    
    git-subtree-dir: seims/src/commonlibs/UtilsClass
    git-subtree-split: eeed8a8525b9bb149fb7906933a4dbc05da542d8

--- doc/ci/travis/linux_gcc48/install.sh
+++ doc/ci/travis/linux_gcc48/install.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
 set -e
-ls
-mkdir utils_linux_gcc48
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DINSTALL_PREFIX=../utils_linux_gcc48
-make -j4
-sudo make install
-cd ..
-# zip the compiled binary
-zip -r utils_linux_gcc48.zip utils_linux_gcc48
-# list all executable file
-ls
-cd utils_linux_gcc48
+sudo pip install codecov
+echo ${PATH}
+echo ${CXX}
+${CXX} --version
+${CXX} -v
+cmake --version
 ls
