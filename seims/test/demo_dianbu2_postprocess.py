286a4ff64715cde19053bc15ea6f16f150115bf2
crazyzlj <crazyzlj@gmail.com>
2017-12-18 19:27:11 +0800

    update run demo data scripts, make it easy to extend

--- seims/test/demo_dianbu2_postprocess.py
+++ seims/test/demo_dianbu2_postprocess.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-  Running SEIMS of demo watershed named dianbu2.
+  Postprecessing for SEIMS. Demo watershed named dianbu2.
 """
 import os
 import sys
@@ -15,8 +15,8 @@ except ImportError:
     from configparser import ConfigParser  # py3
 from pygeoc.utils import UtilClass
 
-from run_seims import MainSEIMS
-from demo_config import ModelPaths
+from postprocess.plot_timeseries import TimeSeriesPlots
+from demo_config import ModelPaths, write_postprocess_config_file
 
 
 def main():
@@ -24,9 +24,10 @@ def main():
     SEIMS_path = os.path.abspath(cur_path + '../../..')
     model_paths = ModelPaths(SEIMS_path, 'dianbu2', 'model_dianbu2_30m_demo')
 
+    # hydrograph, e.g. discharge
     scenario_id = 0
-    seims_obj = MainSEIMS(model_paths.bin_dir, model_paths.model_dir, sceid=scenario_id)
-    seims_obj.run()
+    post_cfg = write_postprocess_config_file(model_paths, 'postprocess_30m_omp.ini', scenario_id)
+    TimeSeriesPlots(post_cfg).workflow()
 
 
 if __name__ == "__main__":
