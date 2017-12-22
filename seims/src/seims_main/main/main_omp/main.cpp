d195cba533ca1eb35ca9ec00568d1480a5d6a646
crazyzlj <crazyzlj@gmail.com>
2017-12-22 17:44:35 +0800

    add more flexible invoke style of seims_omp

--- seims/src/seims_main/main/main_omp/main.cpp
+++ seims/src/seims_main/main/main_omp/main.cpp
@@ -11,88 +11,95 @@
 int main(int argc, const char *argv[]) {
     /// Register GDAL
     GDALAllRegister();
-    /// Read input parameters
+    /// Set default model input parameters.
     string modelPath;
-    int scenarioID = -1;  /// By default, no BMPs Scenario is used, in case of lack of BMPs database.
-    int i = 0;
     int numThread = 1;
     LayeringMethod layeringMethod = UP_DOWN;
     char mongodbIP[16];
     stringcpy(mongodbIP, "127.0.0.1");
     int port = 27017;
+    int scenarioID = -1;  /// By default, no BMPs Scenario is used, in case of lack of BMPs database.
+    int calibrationID = -1; /// By default, no calibration ID is needed.
+    /// Parse input arguments.
+    int i = 0;
     if (argc < 2) {
         cout << "Error: To run the program, use either the Simple Usage option or Usage option as below." << endl;
         goto errexit;
-    } else if (argc > 2) {
-        i = 1;
+    } else if (argc <= 8 && argv[1][0] != '-') {  // old style, i.e., arguments arranged in a fixed order
+        modelPath = argv[1];
+        if (argc >= 3) numThread = atoi(argv[2]);
+        if (argc >= 4) layeringMethod = (LayeringMethod) atoi(argv[3]);
+        if (argc >= 5) stringcpy(mongodbIP, argv[4]);
+        if (argc >= 6) port = atoi(argv[5]);
+        if (argc >= 7) scenarioID = atoi(argv[6]);
+        if (argc >= 8) calibrationID = atoi(argv[7]);
+        i = 9999;  // avoid to run the while-statement
     } else {
-        i = 2;
+        i = 1;
     }
     while (argc > i) {
-        if (PathExists(string(argv[i]))) {
-            modelPath = argv[i];
+        if (StringMatch(argv[i], "-wp")) {
             i++;
+            if (argc > i) {
+                modelPath = argv[i];
+                i++;
+            } else goto errexit;
         }
-        else {
-            cout << "Model folder " << argv[i] << " is not existed!" << endl;
-            goto errexit;
-        }
-        if (argc > i) {
-            if (atoi(argv[i]) > 0) {
+        else if (StringMatch(argv[i], "-thread")) {
+            i++;
+            if (argc > i) {
                 numThread = atoi(argv[i]);
-                assert(numThread >= 1);
                 i++;
-            } else {
-                goto errexit;
-            }
+            } else goto errexit;
         }
-        if (argc > i) {
-            if (atoi(argv[i]) == 0 || atoi(argv[i]) == 1) {
-                if (atoi(argv[i]) == 0) {
-                    layeringMethod = UP_DOWN;
-                } else {
-                    layeringMethod = DOWN_UP;
-                }
+        else if (StringMatch(argv[i], "-lyr")) {
+            i++;
+            if (argc > i) {
+                layeringMethod = (LayeringMethod)atoi(argv[3]);
                 i++;
-            } else {
-                goto errexit;
-            }
+            } else goto errexit;
         }
-        if (argc > i) {
-            if (isIPAddress(argv[i])) {
+        else if (StringMatch(argv[i], "-host")) {
+            i++;
+            if (argc > i) {
                 stringcpy(mongodbIP, argv[i]);
                 i++;
-                if (argc > i && atoi(argv[i]) > 0) {
-                    port = atoi(argv[i]);
-                    assert(port > 0);
-                    i++;
-                } else {
-                    goto errexit;
-                }
-            } else {
-                goto errexit;
-            }
+            } else goto errexit;
         }
-        if (argc > i) {
-            if (atoi(argv[i]) >= 0) {
+        else if (StringMatch(argv[i], "-port")) {
+            i++;
+            if (argc > i) {
+                port = atoi(argv[i]);
+                i++;
+            } else goto errexit;
+        }
+        else if (StringMatch(argv[i], "-sce")) {
+            i++;
+            if (argc > i) {
                 scenarioID = atoi(argv[i]);
                 i++;
-            } else {
-                goto errexit;
-            }
+            }else goto errexit;
+        }
+        else if (StringMatch(argv[i], "-cali")) {
+            i++;
+            if (argc > i) {
+                calibrationID = atoi(argv[i]);
+                i++;
+            } else goto errexit;
         }
     }
-    if (argc == 2) {
-        modelPath = argv[1];
+    /// Check the validation of input arguments
+    if (!PathExists(modelPath)) {
+        cout << "Model folder " << modelPath << " is not existed!" << endl;
+        goto errexit;
     }
-
-    //cout<<modelPath<<endl;
-    //cout<<numThread<<endl;
-    //cout<<layeringMethod<<endl;
-    //cout<<mongodbIP<<":"<<port<<endl;
-    //cout<<scenarioID<<endl;
-    //SetOpenMPThread(2);
-
+    assert(numThread >= 1);
+    if (!isIPAddress(mongodbIP)) {
+        cout << "MongoDB Hostname " << mongodbIP << " is not a valid IP address!" << endl;
+        goto errexit;
+    }
+    assert(port > 0);
+    /// Run model.
     try {
         MainMongoDB(modelPath, mongodbIP, port, scenarioID, numThread, layeringMethod);
     }
@@ -111,17 +118,20 @@ int main(int argc, const char *argv[]) {
     return 0;
 
     errexit:
-    cout << "Simple Usage:\n " << argv[0] << " <ModelPath>" << endl;
-    cout << "\tBy default: " << endl;
-    cout << "\t\tThe threads or processor number is 1." << endl;
-    cout << "\t\tThe Layering Method is 0, which means UP_DOWN." << endl;
-    cout << "\t\tThe MongoDB IP is 127.0.0.1 (i.e., localhost), and the port is 27017." << endl;
-    cout << "\t\tThe Scenario ID is 0" << endl << endl;
-    cout << "Complete Usage: " << argv[0] << " <ModelPath> [<threadsNum> <layeringMethod> <IP> <port> <ScenarioID>]" << endl;
+    cout << "Simple Usage:\n    " << argv[0] << 
+        " <ModelPath> [<threadsNum> <layeringMethod> <IP> <port> <ScenarioID> <CalibrationID>]" << endl;
     cout << "\t<ModelPath> is the path of the configuration of the Model." << endl;
-    cout << "\t<threadsNum> must be greater or equal than 1." << endl;
-    cout << "\t<layeringMethod> can be 0 and 1, which means UP_DOWN and DOWN_UP respectively." << endl;
+    cout << "\t<threadsNum> is thread or processor number, which must be greater or equal than 1 (default)." << endl;
+    cout << "\t<layeringMethod> can be 0 and 1, which means UP_DOWN (default) and DOWN_UP respectively." << endl;
     cout << "\t<IP> is the address of MongoDB database, and <port> is its port number." << endl;
-    cout << "\t<ScenarioID> is the ID of BMPs Scenario which will be defined in BMPs database." << endl;
+    cout << "\t\tBy default, MongoDB IP is 127.0.0.1 (i.e., localhost), and the port is 27017." << endl;
+    cout << "\t<ScenarioID> is the ID of BMPs Scenario which has been defined in BMPs database." << endl;
+    cout << "\t\tBy default, the Scenario ID is -1, which means not used." << endl << endl;
+    cout << "\t<CalibrationID> is the ID of Calibration which has been defined in PARAMETERS table." << endl;
+    cout << "\t\tBy default, the Calibration ID is -1, which means not used." << endl;
+    cout << endl;
+    cout << "Complete and recommended Usage:\n    " << argv[0] << 
+        " -wp <ModelPath> [-thread <threadsNum> -lyr <layeringMethod>"
+        " -host <IP> -port <port> -sce <ScenarioID> -cali <CalibrationID>]" << endl;
     exit(0);
 }
