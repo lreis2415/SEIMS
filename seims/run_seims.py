ecc00cf6941a9a58a45649a0445e9907d728f893
crazyzlj <crazyzlj@gmail.com>
2017-12-23 17:47:09 +0800

    add model output dir

--- seims/run_seims.py
+++ seims/run_seims.py
@@ -17,7 +17,7 @@ class MainSEIMS(object):
     """
 
     def __init__(self, bin_dir, model_dir, nthread=4, lyrmtd=0,
-                 ip='127.0.0.1', port=27017, sceid=0, caliid=-1,
+                 ip='127.0.0.1', port=27017, sceid=-1, caliid=-1,
                  ver='OMP'):
         if ver == 'MPI':  # TODO, MPI version is currently unusable.
             self.seims_exec = bin_dir + os.sep + 'seims_mpi'
@@ -28,22 +28,25 @@ class MainSEIMS(object):
         self.lyrmtd = lyrmtd
         self.host = ip
         self.port = port
-        self.scenario = sceid
+        self.scenario_id = sceid
+        self.calibration_id = caliid
         self.version = ver
         self.run_success = False
         # Concatenate command
         self.cmd = [self.seims_exec,
                     '-wp', self.model_dir, '-thread', str(self.nthread),
                     '-lyr', str(self.lyrmtd), '-host', self.host, '-port', self.port,
-                    '-sce', str(self.scenario)]
-        if caliid >= 0:
-            self.cmd.append('-cali')
-            self.cmd.append(str(caliid))
+                    '-sce', str(self.scenario_id), '-cali', str(self.calibration_id)]
+        self.output_dir = self.model_dir + os.sep + 'OUTPUT'
+        if self.scenario_id >= 0:
+            self.output_dir += str(self.scenario_id)
+        if self.calibration_id >= 0:
+            self.output_dir += '-%d' % self.calibration_id
 
     def run(self):
         # Deprecated by lj.
         # cmd_str = '%s %s %d %d %s %d %d' % (self.seims_exec, self.model_dir, self.nthread,
-        #                                     self.lyrmtd, self.host, self.port, self.scenario)
+        #                                     self.lyrmtd, self.host, self.port, self.scenario_id)
         try:
             UtilClass.run_command(self.cmd)
             self.run_success = True
