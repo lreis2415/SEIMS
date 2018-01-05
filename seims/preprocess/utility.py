9f297b426d18942f51fea3de1c9c474e021a9d07
crazyzlj <crazyzlj@gmail.com>
2018-01-05 22:12:11 +0800

    rm sum_outlet_output, replaced by read_simulation_from_txt

--- seims/preprocess/utility.py
+++ seims/preprocess/utility.py
@@ -52,18 +52,3 @@ def read_data_items_from_txt(txt_file):
                     line_list = StringClass.split_string(str_line, [','])
                 data_items.append(line_list)
     return data_items
-
-
-def sum_outlet_output(rfile):
-    """Sum the output variables in outlet txt file."""
-    data = read_data_items_from_txt(rfile)
-    sum_data = 0.
-    for item in data:
-        item = StringClass.split_string(item[0], ' ', True)
-        if len(item) < 3:
-            continue
-        try:
-            sum_data += float(item[2])
-        except ValueError or Exception:
-            pass
-    return sum_data
