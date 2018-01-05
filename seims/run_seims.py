132688fa87aa9ff3fdf5f0b8d99c523fd55043e5
crazyzlj <crazyzlj@gmail.com>
2018-01-05 22:08:21 +0800

    add some properties which are read from MongoDB

--- seims/run_seims.py
+++ seims/run_seims.py
@@ -10,6 +10,8 @@ from subprocess import CalledProcessError
 
 from pygeoc.utils import UtilClass
 
+from postprocess.load_mongodb import ReadModelData
+
 
 class MainSEIMS(object):
     """
@@ -23,7 +25,7 @@ class MainSEIMS(object):
             self.seims_exec = bin_dir + os.sep + 'seims_mpi'
         else:
             self.seims_exec = bin_dir + os.sep + 'seims_omp'
-        self.model_dir = model_dir
+        self.model_dir = os.path.abspath(model_dir)
         self.nthread = nthread
         self.lyrmtd = lyrmtd
         self.host = ip
@@ -32,27 +34,76 @@ class MainSEIMS(object):
         self.calibration_id = caliid
         self.version = ver
         self.run_success = False
-        # Concatenate command
+        self.cmd = self.Command
+        self.output_dir = self.OutputDirectory
+        # read model data from MongoDB
+        self.spatialdb_name = self.model_dir.split(os.sep)[-1]
+        self.outlet_id = self.OutletID
+        self.start_time, self.end_time = self.SimulatedPeriod
+
+    @property
+    def OutputDirectory(self):
+        self.output_dir = self.model_dir + os.sep + 'OUTPUT'
+        if self.scenario_id >= 0:
+            self.output_dir += str(self.scenario_id)
+        if self.calibration_id >= 0:
+            self.output_dir += '-%d' % self.calibration_id
+        return self.output_dir
+
+    @property
+    def Command(self):
+        """Concatenate command to run SEIMS."""
         self.cmd = [self.seims_exec,
                     '-wp', self.model_dir, '-thread', str(self.nthread),
                     '-lyr', str(self.lyrmtd), '-host', self.host, '-port', self.port]
-        self.output_dir = self.model_dir + os.sep + 'OUTPUT'
         if self.scenario_id >= 0:
             self.cmd.append('-sce')
             self.cmd.append(str(self.scenario_id))
-            self.output_dir += str(self.scenario_id)
         if self.calibration_id >= 0:
             self.cmd.append('-cali')
             self.cmd.append(str(self.calibration_id))
-            self.output_dir += '-%d' % self.calibration_id
+        return self.cmd
+
+    @property
+    def OutletID(self):
+        read_model = ReadModelData(self.host, self.port, self.spatialdb_name)
+        return read_model.OutletID
+
+    @property
+    def SimulatedPeriod(self):
+        read_model = ReadModelData(self.host, self.port, self.spatialdb_name)
+        return read_model.SimulationPeriod
+
+    def ReadOutletObservations(self, vars_list):
+        read_model = ReadModelData(self.host, self.port, self.spatialdb_name)
+        return read_model.Observation(self.outlet_id, vars_list,
+                                      self.start_time, self.end_time)
 
     def run(self):
         """Run SEIMS model"""
-        UtilClass.rmmkdir(self.output_dir)
+        UtilClass.rmmkdir(self.OutputDirectory)
         try:
-            UtilClass.run_command(self.cmd)
+            UtilClass.run_command(self.Command)
             self.run_success = True
         except CalledProcessError or Exception:
             print ('Run SEIMS model failed!')
             self.run_success = False
         return self.run_success
+
+
+if __name__ == '__main__':
+    # test the picklable of MainSEIMS class.
+    import pickle
+
+    bindir = r'D:\compile\bin\seims'
+    modeldir = r'C:\z_data\ChangTing\seims_models_phd\youwuzhen10m_longterm_model'
+    seimsobj = MainSEIMS(bindir, modeldir,
+                         nthread=2, lyrmtd=1,
+                         ip='192.168.253.203', port=27018,
+                         sceid=0, caliid=-1)
+    s = pickle.dumps(seimsobj)
+    # print (s)
+    new_s = pickle.loads(s)
+    print (new_s.output_dir)
+    print (new_s.OutletID)
+    print (str(new_s.start_time), str(new_s.end_time))
