9d15d4a5f4e8c1f6e1b37c8c18da35363407dae2
crazyzlj <crazyzlj@gmail.com>
2018-01-23 17:11:14 +0800

    initial implementation of calibration based on DEAP and SCOOP

---
+++ seims/calibration/config.py
@@ -0,0 +1,143 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Base configuration of Parameter Calibration.
+    @author   : Liangjun Zhu
+    @changelog: 18-1-20  lj - initial implementation.\n
+"""
+import os
+import sys
+import argparse
+
+try:
+    from ConfigParser import ConfigParser  # py2
+except ImportError:
+    from configparser import ConfigParser  # py3
+
+from pygeoc.utils import FileClass, StringClass, UtilClass
+
+if os.path.abspath(os.path.join(sys.path[0], '..')) not in sys.path:
+    sys.path.append(os.path.abspath(os.path.join(sys.path[0], '..')))
+
+
+def get_cali_config():
+    """Parse arguments.
+    Returns:
+        cf: ConfigParse object of *.ini file
+        mtd: Calibration method name, currently, 'nsgaii' is supported.
+    """
+    # define input arguments
+    parser = argparse.ArgumentParser(description="Execute parameters calibration.")
+    parser.add_argument('-ini', type=str, help="Full path of configuration file")
+    # add mutually group
+    psa_group = parser.add_mutually_exclusive_group()
+    psa_group.add_argument('-nsgaii', action='store_true', help='Run NSGA-II method')
+    # parse arguments
+    args = parser.parse_args()
+    ini_file = args.ini
+    psa_mtd = 'nsgaii'  # Default
+    if args.nsgaii:
+        psa_mtd = 'nsgaii'
+    if not FileClass.is_file_exists(ini_file):
+        raise ImportError('Configuration file is not existed: %s' % ini_file)
+    cf = ConfigParser()
+    cf.read(ini_file)
+    return cf, psa_mtd
+
+
+class nsgaiiConfig(object):
+    """NSGA-II related parameters"""
+
+    def __init__(self, cf, wp):
+        self.ngens = 1
+        self.npop = 4
+        self.rcross = 0.75
+        self.rmut = 0.1
+        self.rsel = 0.8
+        if 'NSGAII' in cf.sections():
+            self.ngens = cf.getint('NSGAII', 'generationsnum')
+            self.npop = cf.getint('NSGAII', 'populationsize')
+            self.rcross = cf.getfloat('NSGAII', 'crossoverrate')
+            self.rmut = cf.getfloat('NSGAII', 'mutaterate')
+            self.rsel = cf.getfloat('NSGAII', 'selectrate')
+        else:
+            raise ValueError('[NSGAII] section MUST be existed in *.ini file.')
+        if self.npop % 4 != 0:
+            raise ValueError('PopulationSize must be a multiple of 4.')
+        self.dirname = 'Cali_NSGAII_Gen_%d_Pop_%d' % (self.ngens, self.npop)
+
+        self.out_dir = wp + os.sep + self.dirname
+        UtilClass.rmmkdir(self.out_dir)
+        self.hypervlog = self.out_dir + os.sep + 'hypervolume.txt'
+        self.logfile = self.out_dir + os.sep + 'runtime.log'
+        self.logbookfile = self.out_dir + os.sep + 'logbook.txt'
+
+
+class CaliConfig(object):
+    """Parse parameters calibration configuration of SEIMS project."""
+
+    def __init__(self, cf, method='nsgaii'):
+        """Initialization."""
+        # 1. MongoDB
+        self.hostname = '127.0.0.1'  # localhost by default
+        self.port = 27017
+        self.spatial_db = ''
+        if 'MONGODB' in cf.sections():
+            self.hostname = cf.get('MONGODB', 'hostname')
+            self.port = cf.getint('MONGODB', 'port')
+            self.spatial_db = cf.get('MONGODB', 'spatialdbname')
+        else:
+            raise ValueError('[MONGODB] section MUST be existed in *.ini file.')
+        if not StringClass.is_valid_ip_addr(self.hostname):
+            raise ValueError('HOSTNAME illegal defined in [MONGODB]!')
+
+        # 2. SEIMS_Model
+        self.model_dir = ''
+        self.bin_dir = ''
+        self.nthread = 1
+        self.lyrmethod = 0
+        self.sceid = 0
+        self.param_range_def = 'cali_param_rng.def'
+        if 'SEIMS_Model' not in cf.sections():
+            raise ValueError("[SEIMS_Model] section MUST be existed in *.ini file.")
+        self.model_dir = cf.get('SEIMS_Model', 'model_dir')
+        self.bin_dir = cf.get('SEIMS_Model', 'bin_dir')
+        self.nthread = cf.getint('SEIMS_Model', 'threadsnum')
+        self.lyrmethod = cf.getint('SEIMS_Model', 'layeringmethod')
+        if cf.has_option('SEIMS_Model', 'scenarioid'):
+            self.sceid = cf.getint('SEIMS_Model', 'scenarioid')
+        self.param_range_def = cf.get('SEIMS_Model', 'paramrngdef')
+        self.param_range_def = self.model_dir + os.sep + self.param_range_def
+        tstart = cf.get('SEIMS_Model', 'time_start')
+        tend = cf.get('SEIMS_Model', 'time_end')
+        try:  # UTCTIME
+            self.time_start = StringClass.get_datetime(tstart)
+            self.time_end = StringClass.get_datetime(tend)
+        except ValueError:
+            raise ValueError('The time format MUST be "YYYY-MM-DD" or "YYYY-MM-DD HH:MM:SS".')
+        if self.time_start >= self.time_end:
+            raise ValueError("Wrong time setted in [OPTIONAL_PARAMETERS]!")
+
+        if not (FileClass.is_dir_exists(self.model_dir)
+                and FileClass.is_dir_exists(self.bin_dir)):
+            raise IOError('Please Check Directories defined in [PATH]. '
+                          'BIN_DIR and MODEL_DIR are required!')
+        # 3. optimization algorithm
+        self.opt_mtd = method
+        self.opt = None
+        if self.opt_mtd == 'nsgaii':
+            self.opt = nsgaiiConfig(cf, self.model_dir)
+
+
+if __name__ == '__main__':
+    cf, method = get_cali_config()
+    cfg = CaliConfig(cf, method=method)
+
+    print (cfg)
+
+    # test the picklable of SAConfig class.
+    import pickle
+
+    s = pickle.dumps(cfg)
+    # print (s)
+    new_cfg = pickle.loads(s)
+    print (new_cfg.model_dir)
---
+++ seims/calibration/main.py
@@ -0,0 +1,221 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Calibration by NSGA-II algorithm.
+    @author   : Liangjun Zhu
+    @changelog: 18-1-22  lj - initial implementation.\n
+"""
+import array
+import os
+import random
+import time
+
+import matplotlib
+
+if os.name != 'nt':  # Force matplotlib to not use any Xwindows backend.
+    matplotlib.use('Agg', warn=False)
+import numpy
+from deap import base
+from deap import creator
+from deap import tools
+from deap.benchmarks.tools import hypervolume
+from pygeoc.utils import UtilClass
+
+from config import CaliConfig, get_cali_config
+from calibrate import Calibration, initialize_calibrations, calibration_objectives
+from scenario_analysis.utility import print_message
+from scenario_analysis.userdef import initIterateWithCfg, initRepeatWithCfg
+from scenario_analysis.visualization import plot_pareto_front
+from userdef import write_param_values_to_mongodb
+
+# Definitions, assignments, operations, etc. that will be executed by each worker
+#    when paralleled by SCOOP.
+# Thus, DEAP related operations (initialize, register, etc.) are better defined here.
+
+# Multiobjects: Maximum Nash-Sutcliffe coefficients of discharge and sediment
+multi_weight = (1., 1.)
+creator.create('FitnessMulti', base.Fitness, weights=multi_weight)
+# The FitnessMulti class equals to:
+# class FitnessMulti(base.Fitness):
+#     weights = (1., 1.)
+creator.create('Individual', array.array, typecode='d', fitness=creator.FitnessMulti,
+               gen=-1, id=-1)
+# The Individual class equals to:
+# class Individual(array.array):
+#     gen = -1  # Generation No.
+#     id = -1   # Calibration index of current generation
+#     def __init__(self):
+#         self.fitness = FitnessMulti()
+
+# Register NSGA-II related operations
+toolbox = base.Toolbox()
+toolbox.register('gene_values', initialize_calibrations)
+toolbox.register('individual', initIterateWithCfg, creator.Individual, toolbox.gene_values)
+toolbox.register('population', initRepeatWithCfg, list, toolbox.individual)
+toolbox.register('evaluate', calibration_objectives)
+
+# mate and mutate
+toolbox.register('mate', tools.cxSimulatedBinaryBounded)
+toolbox.register('mutate', tools.mutPolynomialBounded)
+
+toolbox.register('select', tools.selNSGA2)
+
+
+def main(cfg):
+    """Main workflow of NSGA-II based Scenario analysis."""
+    random.seed()
+    print_message('Population: %d, Generation: %d' % (cfg.opt.npop, cfg.opt.ngens))
+
+    # create reference point for hypervolume
+    ref_pt = numpy.array([-500., -100.]) * multi_weight * -1
+
+    stats = tools.Statistics(lambda sind: sind.fitness.values)
+    stats.register('min', numpy.min, axis=0)
+    stats.register('max', numpy.max, axis=0)
+    stats.register('avg', numpy.mean, axis=0)
+    stats.register('std', numpy.std, axis=0)
+
+    logbook = tools.Logbook()
+    logbook.header = 'gen', 'evals', 'min', 'max', 'avg', 'std'
+
+    # Initialize population
+    # pop = toolbox.population(cfg, n=cfg.opt.npop) # Deprecated method!
+    cali_obj = Calibration(cfg)
+    pop = list()
+    param_values = list()
+    for i in range(cfg.opt.npop):
+        ind = creator.Individual(cali_obj.initialize())
+        ind.gen = 0
+        ind.id = i
+        pop.append(ind)
+        param_values.append(ind[:])
+    param_values = numpy.array(param_values)
+    write_param_values_to_mongodb(cfg.hostname, cfg.port, cfg.spatial_db,
+                                  cali_obj.ParamDefs, param_values)
+    # get the low and up bound of calibrated parameters
+    bounds = numpy.array(cali_obj.ParamDefs['bounds'])
+    low = bounds[:, 0]
+    up = bounds[:, 1]
+    low = low.tolist()
+    up = up.tolist()
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
+
+    try:
+        # parallel on multiprocesor or clusters using SCOOP
+        from scoop import futures
+        fitnesses = futures.map(toolbox.evaluate, [cali_obj] * len(invalid_ind), invalid_ind)
+        # print ('parallel-fitnesses: ', fitnesses)
+    except ImportError or ImportWarning:
+        # serial
+        fitnesses = toolbox.map(toolbox.evaluate, [cali_obj] * len(invalid_ind), invalid_ind)
+        # print ('serial-fitnesses: ', fitnesses)
+
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = [fit[0], fit[5]]
+
+    pop = toolbox.select(pop, int(cfg.opt.npop * cfg.opt.rsel))
+    record = stats.compile(pop)
+    logbook.record(gen=0, evals=len(pop), **record)
+    print_message(logbook.stream)
+
+    # Begin the generational process
+    output_str = '### Generation number: %d, Population size: %d ###\n' % (cfg.opt.ngens,
+                                                                           cfg.opt.npop)
+    print_message(output_str)
+    UtilClass.writelog(cfg.opt.logfile, output_str, mode='replace')
+
+    for gen in range(1, cfg.opt.ngens + 1):
+        output_str = '###### Generation: %d ######\n' % gen
+        print_message(output_str)
+
+        offspring = [toolbox.clone(ind) for ind in pop]
+        # method1: use crowding distance (normalized as 0~1) as eta
+        # tools.emo.assignCrowdingDist(offspring)
+        # method2: use the index of individual at the sorted offspring list as eta
+        if len(offspring) >= 2:  # when offspring size greater than 2, mate can be done
+            for i, ind1, ind2 in zip(range(len(offspring) / 2), offspring[::2], offspring[1::2]):
+                if random.random() > cfg.opt.rcross:
+                    continue
+                # if ind1.fitness.crowding_dist == float('inf') and \
+                #         ind2.fitness.crowding_dist == float('inf'):
+                #     eta = 1.
+                # elif ind1.fitness.crowding_dist == float('inf') and \
+                #         ind2.fitness.crowding_dist != float('inf'):
+                #     eta = ind2.fitness.crowding_dist
+                # elif ind1.fitness.crowding_dist != float('inf') and \
+                #         ind2.fitness.crowding_dist == float('inf'):
+                #     eta = ind1.fitness.crowding_dist
+                # else:
+                #     eta = (ind1.fitness.crowding_dist + ind2.fitness.crowding_dist) / 2.
+                eta = i  # method2
+                toolbox.mate(ind1, ind2, eta, low, up)
+                toolbox.mutate(ind1, eta, low, up, cfg.opt.rmut)
+                toolbox.mutate(ind2, eta, low, up, cfg.opt.rmut)
+                del ind1.fitness.values, ind2.fitness.values
+        else:
+            toolbox.mutate(offspring[0], 1., low, up, cfg.opt.rmut)
+            del offspring[0].fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        invalid_ind_size = len(invalid_ind)
+        # Write new calibrated parameters to MongoDB
+        param_values = list()
+        for idx, ind in enumerate(invalid_ind):
+            ind.gen = gen
+            ind.id = idx
+            param_values.append(ind[:])
+        param_values = numpy.array(param_values)
+        write_param_values_to_mongodb(cfg.hostname, cfg.port, cfg.spatial_db,
+                                      cali_obj.ParamDefs, param_values)
+        # print_message('Evaluate pop size: %d' % invalid_ind_size)
+        try:
+            from scoop import futures
+            fitnesses = futures.map(toolbox.evaluate, [cali_obj] * invalid_ind_size, invalid_ind)
+        except ImportError or ImportWarning:
+            fitnesses = toolbox.map(toolbox.evaluate, [cali_obj] * invalid_ind_size, invalid_ind)
+
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = [fit[0], fit[5]]
+
+        # Select the next generation population
+        pop = toolbox.select(pop + offspring, int(cfg.opt.npop * cfg.opt.rsel))
+
+        hyper_str = 'Gen: %d, hypervolume: %f\n' % (gen, hypervolume(pop, ref_pt))
+        print_message(hyper_str)
+        UtilClass.writelog(cfg.opt.hypervlog, hyper_str, mode='append')
+
+        record = stats.compile(pop)
+        logbook.record(gen=gen, evals=len(invalid_ind), **record)
+        print_message(logbook.stream)
+
+        # Create plot
+        plot_pareto_front(pop, cfg.opt.out_dir, gen, 'Pareto frontier of Calibration',
+                          'NSE of discharge',
+                          'NSE of sediment')
+        # save in file
+        output_str += 'generation-calibrationID\tNSE-Q\tNSE-SED\tgene_values\n'
+        for indi in pop:
+            output_str += '%d-%d\t%f\t%f\t%s\n' % (indi.gen, indi.id, indi.fitness.values[0],
+                                                   indi.fitness.values[1], str(indi))
+        UtilClass.writelog(cfg.opt.logfile, output_str, mode='append')
+
+    return pop, logbook
+
+
+if __name__ == "__main__":
+    cf, method = get_cali_config()
+    cfg = CaliConfig(cf, method=method)
+
+    print (cfg)
+
+    print_message('### START TO CALIBRATION OPTIMIZING ###')
+    startT = time.time()
+    fpop, fstats = main(cfg)
+    fpop.sort(key=lambda x: x.fitness.values)
+    print_message(fstats)
+    with open(cfg.opt.logbookfile, 'w') as f:
+        f.write(fstats.__str__())
+
+    endT = time.time()
+    print_message('Running time: %.2fs' % (endT - startT))
