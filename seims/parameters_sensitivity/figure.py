0856e1409666ea11fd98a63e02f8ac631e3e5eda
crazyzlj <crazyzlj@gmail.com>
2018-01-23 17:12:15 +0800

    updata hist plot, add cdf plot

--- seims/parameters_sensitivity/figure.py
+++ seims/parameters_sensitivity/figure.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import LinearLocator
 from pygeoc.utils import UtilClass
 
-
+import numpy
 plt.rcParams['font.family'] = ['Times New Roman']
 plt.rcParams['axes.titlesize'] = 'small'
 plt.rcParams['ytick.labelsize'] = 'x-small'
@@ -54,29 +54,26 @@ def cal_row_col_num(tot):
     return row, col
 
 
-def sample_histograms(fig, input_sample, names, levels, param_dict):
+def sample_histograms(input_sample, names, levels, outpath, outname, param_dict):
     """Plot histograms as subplot.
 
     Args:
-        fig:
         input_sample:
         names:
         levels:
+        outpath:
+        outname:
         param_dict:
 
     Returns:
         subplot list.
     """
+    fig = plt.figure()
     num_vars = len(names)
     row, col = cal_row_col_num(num_vars)
-    out = list()
     for var_idx in range(num_vars):
         ax = fig.add_subplot(row, col, var_idx + 1)
-        out.append(ax.hist(input_sample[:, var_idx],
-                           bins=levels,
-                           normed=False,
-                           label=None,
-                           **param_dict))
+        ax.hist(input_sample[:, var_idx], bins=levels, normed=False, label=None, **param_dict)
         ax.get_yaxis().set_major_locator(LinearLocator(numticks=5))
         ax.get_xaxis().set_major_locator(LinearLocator(numticks=5))
         ax.set_title('%s' % (names[var_idx]))
@@ -91,4 +88,86 @@ def sample_histograms(fig, input_sample, names, levels, param_dict):
                        right='off')
         if var_idx % col:  # labels along the left edge are off
             ax.tick_params(axis='y', labelleft='off')
-    return out
+    plt.tight_layout()
+    save_png_eps(plt, outpath, outname)
+    # close current plot in case of 'figure.max_open_warning'
+    plt.cla()
+    plt.clf()
+    plt.close()
+
+def empirical_cdf(out_values, subsections, input_sample, names, levels,
+                  outpath, outname, param_dict):
+    """Visualize the empirical cumulative distribution function(CDF)
+    of the given variable (x) and subsections of y.
+
+    """
+    # prepare data
+    if not isinstance(out_values, numpy.ndarray):
+        out_values = numpy.array(out_values)
+    out_max = numpy.max(out_values)
+    out_min = numpy.min(out_values)
+    if isinstance(subsections, int):
+        if subsections <= 0:
+            raise ValueError('subsections MUST be a integer greater than 0, or list.')
+        step = (out_max - out_min)/subsections
+        subsections = numpy.arange(out_min, out_max + step, step)
+    if isinstance(subsections, list) and len(subsections) == 1:  # e.g., [0]
+        section_pt = subsections[0]
+        if out_min < section_pt < out_max:
+            subsections = [out_min, section_pt, out_max]
+        else:
+            subsections = [out_min, out_max]
+    labels = list()
+    new_input_sample = list()
+    for i in range(1, len(subsections)):
+        decimal1 = 0 if int(subsections[i - 1]) == float(subsections[i - 1]) else 2
+        decimal2 = 0 if int(subsections[i]) == float(subsections[i]) else 2
+        if out_max == subsections[i] and out_min == subsections[i - 1]:
+            labels.append('%s=<y<=%s' % ('{0:.{1}f}'.format(subsections[i - 1], decimal1),
+                                         '{0:.{1}f}'.format(subsections[i], decimal2)))
+            zone = numpy.where((subsections[i - 1] <= out_values) & (out_values <= subsections[i]))
+        elif out_max == subsections[i]:
+            labels.append('y>=%s' % '{0:.{1}f}'.format(subsections[i - 1], decimal1))
+            zone = numpy.where(subsections[i - 1] <= out_values)
+        elif out_min == subsections[i - 1]:
+            labels.append('y<%s' % ('{0:.{1}f}'.format(subsections[i], decimal2)))
+            zone = numpy.where(out_values < subsections[i])
+        else:
+            labels.append('%s=<y<%s' % ('{0:.{1}f}'.format(subsections[i - 1], decimal1),
+                                        '{0:.{1}f}'.format(subsections[i], decimal2)))
+            zone = numpy.where((subsections[i - 1] <= out_values) & (out_values < subsections[i]))
+        new_input_sample.append(input_sample[zone, :][0])
+
+    fig = plt.figure()
+    num_vars = len(names)
+    row, col = cal_row_col_num(num_vars)
+    for var_idx in range(num_vars):
+        ax = fig.add_subplot(row, col, var_idx + 1)
+        for ii in range(len(labels)-1, -1, -1):
+            ax.hist(new_input_sample[ii][:, var_idx], bins=levels, normed=True,
+                    cumulative=True, label=labels[ii], **param_dict)
+        ax.get_yaxis().set_major_locator(LinearLocator(numticks=5))
+        ax.set_ylim(0, 1)
+        ax.set_title('%s' % (names[var_idx]))
+        ax.get_xaxis().set_major_locator(LinearLocator(numticks=4))
+        ax.tick_params(axis='x',  # changes apply to the x-axis
+                       which='both',  # both major and minor ticks are affected
+                       bottom='on',  # ticks along the bottom edge are off
+                       top='off',  # ticks along the top edge are off
+                       labelbottom='on')  # labels along the bottom edge are off)
+        ax.tick_params(axis='y',  # changes apply to the y-axis
+                       which='major',  # both major and minor ticks are affected
+                       length=3,
+                       right='off')
+        if var_idx % col:  # labels along the left edge are off
+            ax.tick_params(axis='y', labelleft='off')
+        if var_idx == 0:
+            ax.legend(loc='lower right', fontsize='xx-small', framealpha=0.8,
+                      bbox_to_anchor=(1, 0),
+                      borderaxespad=0.2, fancybox=True)
+    plt.tight_layout()
+    save_png_eps(plt, outpath, outname)
+    # close current plot in case of 'figure.max_open_warning'
+    plt.cla()
+    plt.clf()
+    plt.close()
