39cf603553eb88a594f175aef5a225c364db5a74
crazyzlj <crazyzlj@gmail.com>
2018-01-24 14:03:24 +0800

    remove CALI_VALUES before reset

--- seims/calibration/userdef.py
+++ seims/calibration/userdef.py
@@ -13,6 +13,7 @@ def write_param_values_to_mongodb(hostname, port, spatial_db, param_defs, param_
     conn = client.get_conn()
     db = conn[spatial_db]
     collection = db['PARAMETERS']
+    collection.update_many({}, {'$unset': {'CALI_VALUES': ''}})
     for idx, pname in enumerate(param_defs['names']):
         v2str = ','.join(str(v) for v in param_values[:, idx])
         collection.find_one_and_update({'NAME': pname}, {'$set': {'CALI_VALUES': v2str}})
