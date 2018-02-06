1be8bf2eb7bb06e325d6e24ea34b57a0063c8083
crazyzlj <crazyzlj@gmail.com>
2018-02-06 08:46:06 +0800

    refactor

--- seims/src/seims_main/main/main_mpi/main.cpp
+++ seims/src/seims_main/main/main_mpi/main.cpp
@@ -82,92 +82,90 @@ int main(int argc, const char **argv) {
     char processor_name[MPI_MAX_PROCESSOR_NAME];
 
     MPI_Init(NULL, NULL);
+    {
+        MPI_Comm_size(MCW, &numprocs);
+        MPI_Comm_rank(MCW, &rank);
+        MPI_Get_processor_name(processor_name, &nameLen);
+        MPI_Group MPI_GROUP_WORLD, slaveGroup;
+        MPI_Comm slaveComm;
+        MPI_Comm_group(MCW, &MPI_GROUP_WORLD);
 
-    MPI_Comm_size(MCW, &numprocs);
+        // create the master, transfer and calculation groups
+        static int notSlaveRanks[] = { MASTER_RANK };
+        // create slaveGroup which exclude the master rank (i.e., notSlaveRanks)
+        MPI_Group_excl(MPI_GROUP_WORLD, 1, notSlaveRanks, &slaveGroup);
+        MPI_Comm_create(MCW, slaveGroup, &slaveComm);
 
-    MPI_Comm_rank(MCW, &rank);
-
-    MPI_Get_processor_name(processor_name, &nameLen);
-
-    MPI_Group MPI_GROUP_WORLD, slaveGroup;
-    MPI_Comm slaveComm;
-    MPI_Comm_group(MCW, &MPI_GROUP_WORLD);
-
-    // create the master, transfer and calculation groups
-    static int notSlaveRanks[] = {MASTER_RANK};
-    // create slaveGroup which exclude the master rank (i.e., notSlaveRanks)
-    MPI_Group_excl(MPI_GROUP_WORLD, 1, notSlaveRanks, &slaveGroup);
-    MPI_Comm_create(MCW, slaveGroup, &slaveComm);
-
-    if (numprocs < 2) {
-        cout << "The number of processors must be greater than 1!" << endl;
-        MPI_Abort(MCW, 1);
-    }
-    int nSlaves = numprocs - 1;
-    // cout << "nSlaves:" << nSlaves << endl;
-
-    if (rank == MASTER_RANK) {
-        /// connect to mongodb, abort if failed.
-        MongoClient *mclient = MongoClient::Init(input_args->m_host_ip, input_args->m_port);
-        if (nullptr == mclient) {
-            cout << "Connect to MongoDB (" << input_args->m_host_ip
-                 << ":" << input_args->m_port << ") failed!" << endl;
+        if (numprocs < 2) {
+            cout << "The number of processors must be greater than 1!" << endl;
             MPI_Abort(MCW, 1);
         }
-        mongoc_client_t *conn = mclient->getConn();
-
-        // read river topology data
-        map<int, SubbasinStruct *> subbasinMap;
-        set<int> groupSet;
-        //ReadRiverTopology(reachFile, subbasinMap, groupSet);
-        string groupMethod = "KMETIS";
-        ReadReachTopologyFromMongoDB(conn, modelName.c_str(), subbasinMap, groupSet, nSlaves, REACH_KMETIS);
-        if ((size_t) nSlaves != groupSet.size()) {
-            groupSet.clear();
-            groupMethod = "PMETIS";
-            ReadReachTopologyFromMongoDB(conn, modelName.c_str(), subbasinMap, groupSet, nSlaves, REACH_PMETIS);
-            if ((size_t) nSlaves != groupSet.size()) {
-                groupSet.clear();
-                groupMethod = "GROUP";
-                ReadReachTopologyFromMongoDB(conn, modelName.c_str(), subbasinMap, groupSet, nSlaves, REACH_GROUP);
-                if ((size_t) nSlaves != groupSet.size()) {
-                    cout << "The number of slave processes (" << nSlaves << ") is not consist with the group number(" <<
-                         groupSet.size() << ").\n";
-                    exit(-1);
+        int nSlaves = numprocs - 1;
+        // cout << "nSlaves:" << nSlaves << endl;
+        try {
+            if (rank == MASTER_RANK) {
+                /// connect to mongodb, abort if failed.
+                MongoClient *mclient = MongoClient::Init(input_args->m_host_ip, input_args->m_port);
+                if (nullptr == mclient) {
+                    cout << "Connect to MongoDB (" << input_args->m_host_ip
+                        << ":" << input_args->m_port << ") failed!" << endl;
+                    MPI_Abort(MCW, 2);
                 }
+                mongoc_client_t *conn = mclient->getConn();
+
+                // read river topology data
+                map<int, SubbasinStruct *> subbasinMap;
+                set<int> groupSet;
+                //ReadRiverTopology(reachFile, subbasinMap, groupSet);
+                string groupMethod = "KMETIS";
+                ReadReachTopologyFromMongoDB(conn, modelName.c_str(), subbasinMap, groupSet, nSlaves, REACH_KMETIS);
+                if ((size_t)nSlaves != groupSet.size()) {
+                    groupSet.clear();
+                    groupMethod = "PMETIS";
+                    ReadReachTopologyFromMongoDB(conn, modelName.c_str(), subbasinMap, groupSet, nSlaves, REACH_PMETIS);
+                    if ((size_t)nSlaves != groupSet.size()) {
+                        groupSet.clear();
+                        groupMethod = "GROUP";
+                        ReadReachTopologyFromMongoDB(conn, modelName.c_str(), subbasinMap, groupSet, nSlaves, REACH_GROUP);
+                        if ((size_t)nSlaves != groupSet.size()) {
+                            cout << "The number of slave processes (" << nSlaves << ") is not consist with the group number(" <<
+                                groupSet.size() << ").\n";
+                            exit(-1);
+                        }
+                    }
+                }
+                cout << groupMethod << ", Number of Groups:" << groupSet.size() << endl;
+                //try {
+                    MasterProcess(subbasinMap, groupSet, projectPath, outputFile);
+                //}
+                //catch (ModelException e) {
+                //    cout << e.toString() << endl;
+                //}
+                mongoc_uri_destroy(uri);
+                // mongo_destroy(conn);
+
+                //MPI_Finalize();
+                //return 0;
+            }
+            else {
+                CalculateProcess(rank, numprocs, nSlaves, slaveComm, projectPath, modulePath, dbAddress.c_str(), dbPort,
+                    modelName.c_str(), nThread, layeringMethod);
             }
         }
-        cout << groupMethod << ", Number of Groups:" << groupSet.size() << endl;
-        try {
-            MasterProcess(subbasinMap, groupSet, projectPath, outputFile);
-        }
-        catch (ModelException e) {
+        catch (ModelException &e) {
             cout << e.toString() << endl;
+            MPI_Abort(MCW, 3);
         }
-        mongoc_uri_destroy(uri);
-        // mongo_destroy(conn);
-
-        MPI_Finalize();
-        return 0;
+        catch (exception &e) {
+            cout << e.what() << endl;
+            MPI_Abort(MCW, 4);
+        }
+        catch (...) {
+            cout << "Unknown exception occurred!" << endl;
+            MPI_Abort(MCW, 5);
+        }
     }
-    try {
-        CalculateProcess(rank, numprocs, nSlaves, slaveComm, projectPath, modulePath, dbAddress.c_str(), dbPort,
-                         modelName.c_str(), nThread, layeringMethod);
-    }
-    catch (ModelException &e) {
-        cout << e.toString() << endl;
-        exit(EXIT_FAILURE);
-    }
-    catch (exception &e) {
-        cout << e.what() << endl;
-        exit(EXIT_FAILURE);
-    }
-    catch (...) {
-        cout << "Unknown exception occurred!" << endl;
-        exit(EXIT_FAILURE);
-    }
-
+    /// Finalize the MPI environment and exit with success
     MPI_Finalize();
-
     return 0;
 }
