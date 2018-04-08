1414496b7505f8816635f2c7f77251928c4520df
crazyzlj <crazyzlj@gmail.com>
2018-04-08 15:52:06 +0800

    insert path os seims to avoid import error

--- seims/parameters_sensitivity/main.py
+++ seims/parameters_sensitivity/main.py
@@ -9,6 +9,11 @@
 from __future__ import absolute_import
 
 import time
+import os
+import sys
+
+if os.path.abspath(os.path.join(sys.path[0], '..')) not in sys.path:
+    sys.path.insert(0, os.path.abspath(os.path.join(sys.path[0], '..')))
 
 from parameters_sensitivity.sensitivity import Sensitivity
 from parameters_sensitivity.config import get_psa_config, PSAConfig
