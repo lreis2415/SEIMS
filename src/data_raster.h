d662ceecf4fd9ed003777dd6d65c97092ca06214
crazyzlj <crazyzlj@gmail.com>
2018-05-18 08:17:15 +0800

    Squashed 'seims/src/ccgl/' changes from 76e992745..11a86de96
    11a86de96 add parenthesis to avoid confusion
    
    git-subtree-dir: seims/src/ccgl
    git-subtree-split: 11a86de9622b8399b3d37ef9ccf5e7f449e7f996

--- src/data_raster.h
+++ src/data_raster.h
@@ -2517,7 +2517,7 @@ void clsRasterData<T, MASK_T>::MaskAndCalculateValidPosition() {
         mask_->GetRasterPositionData(&n_cells_, &raster_pos_data_);
         store_pos_ = false;
     } else if ((!use_mask_ext_ && !same_extent_with_mask && !calc_pos_) ||
-        (use_mask_ext_ || same_extent_with_mask) && !calc_pos_) {
+        (((use_mask_ext_ || same_extent_with_mask) && (!calc_pos_)))) {
         // reStore raster values as fullsize array
         n_cells_ = GetCols() * GetRows();
         store_fullsize_array = true;
