cfcda53c89f32d08adc24dae6f071e90443a0773
crazyzlj <crazyzlj@gmail.com>
2018-09-01 12:32:23 +0800

    update plot pareto graphs

--- seims/scenario_analysis/visualization.py
+++ seims/scenario_analysis/visualization.py
@@ -33,7 +33,8 @@ from postprocess.utility import save_png_eps
 LFs = ['\r', '\n', '\r\n']
 
 
-def plot_2d_scatter(xlist, ylist, title, xlabel, ylabel, ws, filename, subtitle='', cn=False):
+def plot_2d_scatter(xlist, ylist, title, xlabel, ylabel, ws, filename, subtitle='', cn=False,
+                    xmin=None, xmax=None, xstep=None, ymin=None, ymax=None, ystep=None):
     """
     Todo: The size of the point may be vary with the number of points.
 
@@ -55,6 +56,21 @@ def plot_2d_scatter(xlist, ylist, title, xlabel, ylabel, ws, filename, subtitle=
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.scatter(xlist, ylist, c='r', alpha=0.8, s=12)
+    if xmax is not None:
+        plt.xlim(xmax=xmax)
+    if xmin is not None:
+        plt.xlim(xmin=xmin)
+    if xstep is not None:
+        xmin, xmax = plt.xlim()
+        plt.xticks(numpy.arange(xmin, xmax + xstep * 0.99, step=xstep))
+    if ymax is not None:
+        plt.ylim(ymax=ymax)
+    if ymin is not None:
+        plt.ylim(ymin=ymin)
+    if ystep is not None:
+        ymin, ymax = plt.ylim()
+        plt.yticks(numpy.arange(ymin, ymax + ystep * 0.99, step=ystep))
+
     if subtitle != '':
         plt.title(subtitle, color='green', fontsize=9, loc='right')
     plt.tight_layout()
@@ -66,7 +82,9 @@ def plot_2d_scatter(xlist, ylist, title, xlabel, ylabel, ws, filename, subtitle=
 
 
 def plot_3d_scatter(xlist, ylist, zlist, title, xlabel, ylabel, zlabel,
-                    ws, filename, subtitle='', cn=False):
+                    ws, filename, subtitle='', cn=False,
+                    xmin=None, xmax=None, ymin=None, ymax=None, zmin=None, zmax=None,
+                    xstep=None, ystep=None, zstep=None):
     """
 
     Args:
@@ -91,6 +109,28 @@ def plot_3d_scatter(xlist, ylist, zlist, title, xlabel, ylabel, zlabel,
     ax.set_ylabel(ylabel)
     ax.set_zlabel(zlabel)
     ax.scatter(xlist, ylist, zlist, c='r', s=12)
+    if xmax is not None:
+        ax.set_xlim(right=xmax)
+    if xmin is not None:
+        ax.set_xlim(left=xmin)
+    if xstep is not None:
+        xmin, xmax = ax.get_xlim()
+        ax.set_xticks(numpy.arange(xmin, xmax + xstep * 0.99, step=xstep))
+    if ymax is not None:
+        ax.set_ylim(top=ymax)
+    if ymin is not None:
+        ax.set_ylim(bottom=ymin)
+    if ystep is not None:
+        ymin, ymax = ax.get_ylim()
+        ax.set_yticks(numpy.arange(ymin, ymax + ystep * 0.99, step=ystep))
+    if zmax is not None:
+        ax.set_zlim3d(top=zmax)
+    if zmin is not None:
+        ax.set_zlim3d(bottom=zmin)
+    if xstep is not None:
+        zmin, zmax = ax.get_zlim()
+        ax.set_zticks(numpy.arange(zmin, zmax + zstep * 0.99, step=zstep))
+
     if subtitle != '':
         plt.title(subtitle, color='green', fontsize=9, loc='right')
     plt.tight_layout()
@@ -101,7 +141,8 @@ def plot_3d_scatter(xlist, ylist, zlist, title, xlabel, ylabel, zlabel,
     plt.close()
 
 
-def plot_pareto_front(data, labels, ws, gen_id, title, cn=False):
+def plot_pareto_front(data, labels, ws, gen_id, title,
+                      lowers=None, uppers=None, steps=None, cn=False):
     """
     Plot 2D or 3D pareto front graphs.
     Args:
@@ -110,17 +151,30 @@ def plot_pareto_front(data, labels, ws, gen_id, title, cn=False):
         ws: Workspace path
         gen_id: Generation ID
         title: Title
+        lowers: (Optional) Lower values of each axis. Default is None.
+        uppers: (Optional) Upper values of each axis. Default is None.
+        steps: (Optional) Major ticks of each axis. Default is None.
         cn: (Optional) Use Chinese
     """
     if not isinstance(data, numpy.ndarray):
         data = numpy.array(data)
     pop_size, axis_size = data.shape
     if axis_size <= 1:
-        print('Warning: The size of fitness values MUST >= 2 to plot 2D graphs!')
+        print('Error: The size of fitness values MUST >= 2 to plot 2D graphs!')
         return
-    if axis_size != len(labels):
-        print('Warning: The size of fitness values and labels are not consistent!')
+    if len(labels) != axis_size:
+        print('Error: The size of fitness values and labels are not consistent!')
         return
+    if lowers is not None and len(lowers) != axis_size:
+        print('Warning: The size of fitness values and lowers are not consistent!')
+        lowers = None
+    if uppers is not None and len(uppers) != axis_size:
+        print('Warning: The size of fitness values and uppers are not consistent!')
+        uppers = None
+    if steps is not None and len(steps) != axis_size:
+        print('Warning: The size of fitness values and steps are not consistent!')
+        steps = None
+
     subtitle = '\nGeneration: %d, Population: %d' % (gen_id, pop_size)
     if cn:
         subtitle = u'\n代数: %d, 个体数: %d' % (gen_id, pop_size)
@@ -129,12 +183,33 @@ def plot_pareto_front(data, labels, ws, gen_id, title, cn=False):
     for comb in comb_2d:
         x_idx = comb[0]
         y_idx = comb[1]
+        x_min = None
+        x_max = None
+        x_step = None
+        y_min = None
+        y_max = None
+        y_step = None
+        if lowers is not None:
+            x_min = lowers[x_idx]
+            y_min = lowers[y_idx]
+        if uppers is not None:
+            x_max = uppers[x_idx]
+            y_max = uppers[y_idx]
+        if steps is not None:
+            x_step = steps[x_idx]
+            y_step = steps[y_idx]
         maintitle = '%s of (%s, %s)' % (title, labels[x_idx], labels[y_idx])
+        dirname = 'Pareto_%s-%s' % (labels[x_idx], labels[y_idx])
+        tmpws = ws + os.sep + dirname
+        if not os.path.exists(tmpws):
+            os.mkdir(tmpws)
         filename = 'Pareto_Gen_%d_Pop_%d_%s-%s' % (gen_id, pop_size, labels[x_idx], labels[y_idx])
         if cn:
             filename += '_cn'
         plot_2d_scatter(data[:, x_idx], data[:, y_idx], maintitle,
-                        labels[x_idx], labels[y_idx], ws, filename, subtitle, cn)
+                        labels[x_idx], labels[y_idx], tmpws, filename, subtitle, cn=cn,
+                        xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max,
+                        xstep=x_step, ystep=y_step)
     if axis_size >= 3:
         # 3D plot
         comb_3d = list(itertools.combinations(range(axis_size), 3))
@@ -142,17 +217,56 @@ def plot_pareto_front(data, labels, ws, gen_id, title, cn=False):
             x_idx = comb[0]
             y_idx = comb[1]
             z_idx = comb[2]
+            x_min = None
+            x_max = None
+            x_step = None
+            y_min = None
+            y_max = None
+            y_step = None
+            z_min = None
+            z_max = None
+            z_step = None
+            if lowers is not None:
+                x_min = lowers[x_idx]
+                y_min = lowers[y_idx]
+                z_min = lowers[z_idx]
+            if uppers is not None:
+                x_max = uppers[x_idx]
+                y_max = uppers[y_idx]
+                z_max = uppers[z_idx]
+            if steps is not None:
+                x_step = steps[x_idx]
+                y_step = steps[y_idx]
+                z_step = steps[z_idx]
             maintitle = '%s of (%s, %s, %s)' % (title, labels[x_idx], labels[y_idx], labels[z_idx])
+            dirname = 'Pareto_%s-%s-%s' % (labels[x_idx], labels[y_idx], labels[z_idx])
+            tmpws = ws + os.sep + dirname
+            if not os.path.exists(tmpws):
+                os.mkdir(tmpws)
             filename = 'Pareto_Gen_%d_Pop_%d_%s-%s-%s' % (gen_id, pop_size, labels[x_idx],
                                                           labels[y_idx], labels[z_idx])
             if cn:
                 filename += '_cn'
             plot_3d_scatter(data[:, x_idx], data[:, y_idx], data[:, z_idx], maintitle,
                             labels[x_idx], labels[y_idx], labels[z_idx],
-                            ws, filename, subtitle, cn)
+                            tmpws, filename, subtitle, cn=cn,
+                            xmin=x_min, xmax=x_max, ymin=y_min, ymax=y_max, zmin=z_min, zmax=z_max,
+                            xstep=x_step, ystep=y_step, zstep=z_step)
 
 
-def read_pareto_points_from_txt(txt_file, sce_name, xname, yname):
+def read_pareto_points_from_txt(txt_file, sce_name, headers, labels=None):
+    """Read Pareto points from `runtime.log` file.
+
+    Args:
+        txt_file: Full file path of `runtime.log` output by NSGA2 algorithm.
+        sce_name: Field name followed by `generation`, e.g., 'calibrationID', 'scenarioID', etc.
+        headers: Filed names in header for each dimension of Pareto front
+        labels: (Optional) Labels corresponding to `headers` for Pareto graphs
+
+    Returns:
+        pareto_points: `OrderedDict`, key is generation ID, value is Pareto front array
+        pareto_popnum: `OrderedDict`, key is generation ID, value is newly model runs number
+    """
     with open(txt_file) as f:
         lines = f.readlines()
     pareto_points = OrderedDict()
@@ -160,9 +274,17 @@ def read_pareto_points_from_txt(txt_file, sce_name, xname, yname):
     found = False
     cur_gen = -1
     iden_idx = -1
-    xidx = -1
-    yidx = -1
-    for line in lines:
+
+    new_headers = headers[:]
+    for i, hd in enumerate(new_headers):
+        new_headers[i] = hd.upper()
+    if labels is None:
+        labels = headers[:]
+
+    headers_idx = list()
+    new_labels = list()
+
+    for lno, line in enumerate(lines):
         str_line = line
         for LF in LFs:
             if LF in line:
@@ -173,46 +295,45 @@ def read_pareto_points_from_txt(txt_file, sce_name, xname, yname):
         if str_line[0] == '#' and 'Generation' in str_line:
             if len(values) != 1:
                 continue
+            # e.g., ###### Generation: 23 ######
             gen = int(values[0])
-            # if gen not in gens:
-            #     found = False
-            #     continue
             found = True
             cur_gen = gen
             pareto_popnum[cur_gen] = list()
-            pareto_points[cur_gen] = {xname[0]: list(), yname[0]: list()}
+            pareto_points[cur_gen] = list()
             continue
-        if not found:
+        if not found:  # If the first "###### Generation: 1 ######" has not been found.
             continue
+        line_list = StringClass.split_string(str_line.upper(), ['\t'])
         if values is None:  # means header line
-            line_list = StringClass.split_string(str_line, ['\t'])
+            if headers_idx and new_labels:
+                continue
             for idx, v in enumerate(line_list):
-                if StringClass.string_match(v, sce_name):
+                if sce_name.upper() in v.upper():
                     iden_idx = idx
                     break
-            for idx, v in enumerate(line_list):
-                if StringClass.string_match(v, xname[0]):
-                    xidx = idx
-                if StringClass.string_match(v, yname[0]):
-                    yidx = idx
+            for fldno, fld in enumerate(new_headers):
+                if fld in line_list:
+                    tmpidx = line_list.index(fld)
+                    headers_idx.append(tmpidx)
+                    new_labels.append(labels[fldno])
             continue
-        if xidx < 0 or yidx < 0 or iden_idx < 0:
+        if iden_idx < 0:
             continue
         # now append the real Pareto front point data
-        pareto_points[cur_gen][xname[0]].append(values[xidx])
-        pareto_points[cur_gen][yname[0]].append(values[yidx])
-        pareto_popnum[cur_gen].append(int(values[iden_idx]))
+        tmpvalues = list()
+        for tmpidx in headers_idx:
+            tmpvalues.append(StringClass.extract_numeric_values_from_string(line_list[tmpidx])[0])
+        pareto_points[cur_gen].append(tmpvalues[:])
+        iden_str = line_list[iden_idx]  # e.g., 1-44
+        iden_strs = iden_str.split('-')
+        if len(iden_strs) == 1:
+            pareto_popnum[cur_gen].append(int(iden_strs[0]))
+        if len(iden_strs) == 2:
+            pareto_popnum.setdefault(int(iden_strs[0]), list())
+            pareto_popnum[int(iden_strs[0])].append(int(iden_strs[1]))
 
-    all_sceids = list()
-    acc_num = list()
-    genids = sorted(list(pareto_popnum.keys()))
-    for idx, genid in enumerate(genids):
-        for _id in pareto_popnum[genid]:
-            if _id not in all_sceids:
-                all_sceids.append(_id)
-        acc_num.append(len(all_sceids))
-
-    return pareto_points, (genids, acc_num)
+    return pareto_points, pareto_popnum
 
 
 def read_pareto_popsize_from_txt(txt_file, sce_name='scenario'):
@@ -484,7 +605,7 @@ def plot_hypervolume_single(hypervlog, ws=None, cn=False):
         ax2.set_ylabel(nmodel_str)
         p2 = ax2.plot(x, nmodel, linestyle=linestyles[0], marker=markers[mark_idx],
                       color='black', label=nmodel_str, linewidth=2, markersize=4)
-        legends +=p2
+        legends += p2
 
     legends_label = [l.get_label() for l in legends]
     ax.legend(legends, legends_label, loc='center right')
