a04d8a5e8f7a9ea8cc95d59eb0693cfb09160ef9
ShenShen <dslwz2008@gmail.com>
2021-06-04 17:28:51 +0800

    modify sed sum ave calculation

--- seims/calc_sed_sum.py
+++ seims/calc_sed_sum.py
@@ -5,26 +5,37 @@ from pygeoc.utils import FileClass, StringClass, UtilClass, get_config_parser, i
 
 MODEL_PATH = 'D:/Programs/SEIMS/data/youwuzhen/ss_youwuzhen10m_longterm_model'
 
+
 def calc_sed_sum(sceid, impl_period):
-    raster_file = '%s/OUTPUT%d/SED_OL_SUM.tif' % (MODEL_PATH,sceid)
+    raster_file = '%s/OUTPUT%d/SED_OL_SUM.tif' % (MODEL_PATH, sceid)
     rr = RasterUtilClass.read_raster(raster_file)
-    sed_sum = rr.get_sum() / impl_period
-    print(sed_sum)
+    sed_sum = rr.get_sum()
+    print('总产沙量：%f' % (sed_sum,))
+
+    raster_file = '%s/OUTPUT%d/SED_OL_AVE.tif' % (MODEL_PATH, sceid)
+    rr = RasterUtilClass.read_raster(raster_file)
+    sed_sum = rr.get_sum()
+    print('日平均产沙量：%f' % (sed_sum,))
 
     raster_file = '%s/OUTPUT%d/1_SED_OL_SUM.tif' % (MODEL_PATH, sceid)
     rr = RasterUtilClass.read_raster(raster_file)
-    sed_sum = rr.get_sum() / impl_period
-    print(sed_sum)
+    sed_sum = rr.get_sum()
+    print('13年产沙量：%f' % (sed_sum,))
 
     raster_file = '%s/OUTPUT%d/2_SED_OL_SUM.tif' % (MODEL_PATH, sceid)
     rr = RasterUtilClass.read_raster(raster_file)
-    sed_sum = rr.get_sum() / impl_period
-    print(sed_sum)
+    sed_sum = rr.get_sum()
+    print('14年产沙量：%f' % (sed_sum,))
 
     raster_file = '%s/OUTPUT%d/3_SED_OL_SUM.tif' % (MODEL_PATH, sceid)
     rr = RasterUtilClass.read_raster(raster_file)
-    sed_sum = rr.get_sum() / impl_period
-    print(sed_sum)
+    sed_sum = rr.get_sum()
+    print('15年产沙量：%f' % (sed_sum,))
+
+    raster_file = '%s/OUTPUT%d/4_SED_OL_SUM.tif' % (MODEL_PATH, sceid)
+    rr = RasterUtilClass.read_raster(raster_file)
+    sed_sum = rr.get_sum()
+    print('12年产沙量：%f' % (sed_sum,))
 
 
 if __name__ == '__main__':
