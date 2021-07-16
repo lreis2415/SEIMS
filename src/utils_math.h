d37ba179e3c5ef73f29abd437a06376aeed39bb9
crazyzlj <crazyzlj@gmail.com>
2021-07-16 21:32:18 +0800

    Squashed 'seims/src/ccgl/' changes from f18f2592c..99a16a73b
    99a16a73b integrate fast pow,exp,ln,sqrt of PAL library
    b19b1ff77 integrate fast pow,exp,ln,sqrt of PAL library
    
    git-subtree-dir: seims/src/ccgl
    git-subtree-split: 99a16a73be492539dcb2e599ad1436f1e1a8df37

--- src/utils_math.h
+++ src/utils_math.h
@@ -4,9 +4,10 @@
  *
  * Changelog:
  *   - 1. 2018-05-02 - lj - Make part of CCGL.
+ *   - 2. 2021-07-15 - lj - Integrate pal.math for fast pow, exp, and ln
  *
  * \author Liangjun Zhu (zlj@lreis.ac.cn)
- * \version 1.0
+ * \version 1.1
  */
 #ifndef CCGL_UTILS_MATH_H
 #define CCGL_UTILS_MATH_H
@@ -15,6 +16,76 @@
 
 #include "basic.h"
 #include "utils_array.h"
+
+#define M_E        2.7182818284590452354     /* e */
+#define M_LOG2E    1.4426950408889634074     /* log 2e */
+#define M_LOG10E   0.43429448190325182765    /* log 10e */
+#ifndef M_LN2 /* Avoid warning, newlib defines this as _M_LN2 */
+#define M_LN2      0.69314718055994530942    /* log e2 */
+#endif
+#define M_LN10     2.30258509299404568402    /* log e10 */
+#define M_PI       3.14159265358979323846    /* pi */
+#define M_PI_2     1.57079632679489661923    /* pi/2 */
+#define M_PI_4     0.78539816339744830962    /* pi/4 */
+#define M_1_PI     0.31830988618379067154    /* 1/pi */
+#define M_2_PI     0.63661977236758134308    /* 2/pi */
+#define M_2_SQRTPI 1.12837916709551257390    /* 2/sqrt(pi) */
+#define M_SQRT2    1.41421356237309504880    /* sqrt(2) */
+#define M_SQRT1_2  0.70710678118654752440    /* 1/sqrt(2) */
+#define M_2POW23   8388608.0f                /* pow(2, 23) */
+
+#ifndef HUGE
+#define HUGE ((float)3.40282347e+38)      /*maximum value of float*/
+#endif
+#ifndef MAXFLOAT
+#define MAXFLOAT ((float)3.40282347e+38)  /*maximum value of float*/
+#endif
+#ifndef MINFLOAT
+#define MINFLOAT ((float)1.175494351e-38)  /*minimum value of float*/
+#endif
+
+/* This define is in newlib's math.h (but nowhere else) */
+#ifndef M_SQRT3
+#define M_SQRT3 1.732051f                  /* sqrt(3) */
+#endif
+/*ADDED MATH CONSTANTS*/
+#define M_TC     0.63212055882855767840f    /* 1 - 1/e */
+#define M_PI2    6.283185f                  /* pi*2 */
+#define M_GOLDEN 1.618034f                  /* golden ratio */
+#define M_1_PI2  0.15915494309189534561f    /* 1/(pi*2) */
+
+/*ADDED RECIPROCAL CONSTANTS (AVOID DIVISION AT ALL COST)*/
+/*IDEALLY THIS WOULD BE IN THE COMPILER? A BETTER WAY?*/
+#define M_DIV3  0.3333333333333333333f  /* 1/3 */
+#define M_DIV4  0.25f                   /* 1/4 */
+#define M_DIV5  0.2f                    /* 1/5 */
+#define M_DIV6  0.1666666666666666666f  /* 1/6 */
+#define M_DIV7  0.142857143f            /* 1/7 */
+#define M_DIV8  0.125f                  /* 1/8 */
+#define M_DIV9  0.1111111111111111111f  /* 1/9 */
+#define M_DIV10 0.1f                    /* 1/10 */
+#define M_DIV11 0.090909091f            /* 1/11 */
+#define M_DIV12 0.0833333333333333333f  /* 1/12 */
+#define M_DIV13 0.076923077f            /* 1/13 */
+#define M_DIV14 0.071428571f            /* 1/14 */
+#define M_DIV15 0.0666666666666666666f  /* 1/15 */
+#define M_DIV16 0.0625f                 /* 1/16 */
+#define M_DIV_LN10 0.43429448190325182765f /* 1 / ln(10) */
+
+/*ADDED PHYSICAL CONSTANTS (ADD YOUR FAVORITES:-)*/
+#define PH_C    ((float)299792458)             /*speed of light (m/s)*/
+#define PH_M0   ((float)1.2566370614359172950) /*mag permeability (mH/m)*/
+#define PH_H    ((float)6.62606957e-34)        /*planck constant (J/Hz)*/
+#define PH_HBAR ((float)1.05457172e-34)        /*diract constant* (J.s/rad)*/
+#define PH_K    ((float)1.3806488e-23)         /*boltzmann constant (J/K)*/
+#define PH_ME   ((float)9.10938291e-31)        /*mass of electron (kg)*/
+#define PH_MP   ((float)1.672614e-27)          /*mass of proton (kg)*/
+#define PH_MN   ((float)1.674920e-27)          /*mass of neutron (kg)*/
+#define PH_EC   ((float)1.6021917e-19)         /*charge of electron (C)*/
+#define PH_F    ((float)9.648670e4)            /*faraday constant (C/mol)*/
+#define PH_G    ((float)6.6732e-11)            /*gravitational constant (N*m^2/kg^2)*/
+#define PH_AVO  ((float)6.022169e23)           /*avogadro constant*/
+
 
 namespace ccgl {
 /*!
@@ -115,6 +186,136 @@ void BasicStatistics(const T* values, int num, double** derivedvalues,
 template <typename T>
 void BasicStatistics(const T*const * values, int num, int lyrs,
                      double*** derivedvalues, T exclude = static_cast<T>(NODATA_VALUE));
+
+/*!
+ * \brief approximate sqrt
+ *
+ * This uses a method to approximate sqrt which only applies to IEEE 754 floating point numbers,
+ * described in [1]. The optimized magic constant is from Chris Lomont[2]
+ *
+ * References:
+ * 1: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+ * 2: http://www.lomont.org/Math/Papers/2003/InvSqrt.pdf
+ *
+ * \param[in] z float or double value
+ * \return approximation of sqrt
+ */
+float ApprSqrt(float z);
+double ApprSqrt(double z);
+
+template<typename T>
+T CalSqrt(T val) {
+#if defined(USE_NATIVE_MATH)
+    return sqrt(val);
+#elif defined(USE_APPR_PAL_MATH)
+    return ApprSqrt(val);
+#else
+    return ApprSqrt(val);
+#endif
+}
+
+/*!
+ * \brief Approximate e^x by Taylor Series expansion when 0<=x<=ln2
+ *
+ * e^x = 1./e^-x
+ * e^-x = 1 + a1 * x + a2 * x^2 + ... + a4 * x^4 + e(x), where |e(x)| <= 3 * 10^-5
+ *
+ */
+template <typename T>
+static inline T __p_exp_ln2(const T x) {
+    const T a1 = static_cast<T>(-0.9998684);
+    const T a2 = static_cast<T>(0.4982926);
+    const T a3 = static_cast<T>(-0.1595332);
+    const T a4 = static_cast<T>(0.0293641);
+    T exp_x = static_cast<T>(1.0) +
+        a1 * x +
+        a2 * x * x +
+        a3 * x * x * x +
+        a4 * x * x * x * x;
+    return static_cast<T>(1.0) / exp_x;
+}
+
+/*!
+ * \brief Approximate e^x when x >= 0
+ *
+ * exp x = exp(x' + k ln 2) = (exp x') * 2^k
+ */
+template <typename T>
+static inline T __p_exp_pos(const T x) {
+    long int k, twok;
+    static const T ln2 = static_cast<T>(M_LN2);
+    T x_;
+    k = x / ln2;
+    twok = 1ULL << k;
+    x_ = x - static_cast<T>(k) * ln2;
+    return static_cast<T>(twok) * __p_exp_ln2(x_);
+}
+
+template <typename T>
+static inline T ApprExp(const T x) {
+    if (x >= static_cast<T>(0.0))
+        return __p_exp_pos(x);
+    else
+        return static_cast<T>(1.0) / __p_exp_pos(-x);
+}
+
+template<typename T>
+T CalExp(T val) {
+#if defined(USE_NATIVE_MATH)
+    return exp(val);
+#elif defined(USE_APPR_PAL_MATH)
+    return ApprExp(val);
+#else
+    return ApprExp(val);
+#endif
+}
+
+/*!
+ * \brief Approximates the natural logarithm, (where the base is 'e'=2.71828)
+ *
+ */
+float ApprLn(float z);
+double ApprLn(double z);
+
+template<typename T>
+T CalLn(T val) {
+#if defined(USE_NATIVE_MATH)
+    return log(val);
+#elif defined(USE_APPR_PAL_MATH)
+    return ApprLn(val);
+#else
+    return ApprLn(val);
+#endif
+}
+
+/*!
+  * \brief lookup for pow(a, b) function
+  * \param[in] exp power to raise radix to (exponent), i.e., b in pow(a, b)
+  * \param[in] log_base one over log, to required radix, of two (ln(base))
+  */
+float pow_lookup(const float exp, const float log_base);
+
+/*!
+ * \brief Approximates pow(a, b) based on the work of Harrison Ainsworth.
+ *
+ * Refers to http://www.hxa.name/articles/content/fast-pow-adjustable_hxa7241_2007.html
+ * Copyright (c) 2007, Harrison Ainsworth / HXA7241.
+ */
+float inline ApprPow(float a, float b) {
+    // pow(base, exponent) = pow_lookup(exponent, ln(base))
+    return pow_lookup(b, ApprLn(a));
+};
+
+template<typename T>
+T CalPow(T a, T b) {
+#if defined(USE_NATIVE_MATH)
+    return pow(a,b);
+#elif defined(USE_APPR_PAL_MATH)
+    return ApprPow(CVT_FLT(a), CVT_FLT(b));
+#else
+    return ApprPow(CVT_FLT(a), CVT_FLT(b));
+#endif
+}
 
 /************ Implementation of template functions ******************/
 template <typename T>
@@ -240,6 +441,7 @@ void BasicStatistics(const T*const * values, const int num, const int lyrs,
     utils_array::Release1DArray(sumv);
     *derivedvalues = tmpstats;
 }
+
 } /* namespace: utils_math */
 } /* namespace: ccgl */
 
