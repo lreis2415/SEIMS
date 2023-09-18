60ddbcd687823a034065517042a26206c89f4c80
Wang Yu-Jing <swordoom.g@gmail.com>
2023-09-18 22:25:56 +0800

    draft: 尝试换py2，但环境老是出问题

--- seims/test_conceptual/rerun.py
+++ seims/test_conceptual/rerun.py
@@ -23,12 +23,12 @@ def rerun(config_files_dir):
     parser1.read(preprocess_config_file)
     preprocess_config = PreprocessConfig(parser1)
 
-    logging.info(f'Using %s' % run_config_file)
+    logging.info('Using %s' % run_config_file)
     parser2 = ConfigParser()
     parser2.read(run_config_file)
     run_config = ParseSEIMSConfig(parser2)
 
-    logging.info(f'Using %s' % postprocess_config_file)
+    logging.info('Using %s' % postprocess_config_file)
     parser3 = ConfigParser()
     parser3.read(postprocess_config_file)
     postprocess_config = PostConfig(parser3)
