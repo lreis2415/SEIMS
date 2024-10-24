321e0c360a2e8118a97b2b081286fa3c75c294c6
Liang-Jun Zhu <crazyzlj@gmail.com>
2024-10-24 19:45:10 +0800

    Update gdal version

--- .github/workflows/download_gdal.ps1
+++ .github/workflows/download_gdal.ps1
@@ -1,4 +1,4 @@
-﻿param ($gdalPath = "$env:SystemDrive\gdal", $VSversion = "1928", $GDALversion = "3.5.3", $MAPSversion = "8.0.0")
+﻿param ($gdalPath = "$env:SystemDrive\gdal", $VSversion = "1928", $GDALversion = "3.9.1", $MAPSversion = "8.2.0")
 $GDALversion=$GDALversion -replace '\.','-'
 $MAPSversion=$MAPSversion -replace '\.','-'
 $urllib = "https://download.gisinternals.com/sdk/downloads/release-$VSversion-x64-gdal-$GDALversion-mapserver-$MAPSversion-libs.zip"
@@ -24,7 +24,9 @@ Write-Host "Setting environmetal paths of GDAL……"
 $env:GDAL_ROOT = $gdalPath
 $env:GDAL_DATA = "$gdalPath\bin\gdal-data"
 $env:GDAL_BIN = "$gdalPath\bin;$gdalPath\include;$gdalPath\lib;$gdalPath\bin\gdal\apps;$gdalPath\bin\gdal\java;$gdalPath\bin\proj\apps;$gdalPath\bin\curl;"
+$env:PATH = "$env:GDAL_BIN;$env:PATH"
 
-Write-Output "GDAL_DIR=$env:GDAL_ROOT"
+Write-Output "GDAL_ROOT=$env:GDAL_ROOT"
 Write-Output "GDAL_DATA=$env:GDAL_DATA"
-Write-Output "PATH=$env:GDAL_BIN"
+Write-Output "GDAL_BIN=$env:GDAL_BIN"
+Write-Output "PATH=$env:PATH"
