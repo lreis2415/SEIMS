21a01598fb1eac704d98ac7eb268d216d4f2c4ca
Liang-Jun Zhu <crazyzlj@gmail.com>
2025-09-07 17:36:39 -0500

    Squashed 'seims/src/ccgl/' changes from 4aac4101..8d4cb5b6
    8d4cb5b6 Merge pull request #19 from crazyzlj/dev [ci skip]
    82132370 update to the latest master
    12461887 add #ifdef USE_GDAL typo fixed
    dc1a7de1 Add header of simple wrappers of GDAL API that compatible with all versions
    a2b472a3 Update Windows version for GitActions
    7667d15c Merge pull request #18 from crazyzlj/bug/setnodata_after_copyheader
    83bfa071 Use macos-13-xlarge and mongodb 7.0 for most compatibility
    e5970e13 Add argument: -opts
    8ac0be74 Add specific header: PARAM_ABSTRACTION_TYPE
    4f7cbf07 Add Read2DArrayFromCsvFile
    78fb4fd6 Fix FindFiles function
    8b8f85eb Add function of IsVectorAllSame
    432ab125 Add function of UpdateStringMapIfNotExist
    f55a618e [ci skip] Merge commit '8d31c4cf074efa132e8f8b4ea083507b2fa634da' into dev
    8d31c4cf Squashed 'cmake/' changes from 2c2cf3f..2c05601
    
    git-subtree-dir: seims/src/ccgl
    git-subtree-split: 8d4cb5b619db5ecec7376cf74bbfe5a35c7de362

---
+++ src/gdal_handler.h
@@ -0,0 +1,101 @@
+/*!
+* \file gdal_handler.h
+ * \brief Simple wrappers of GDAL API that compatible with all versions
+ *
+ * \remarks
+ *   - 1. 2025-09-05 - lj - Initial implementation.
+ *
+ * \note No exceptions will be thrown.
+ * \author Liangjun Zhu, zlj(at)lreis.ac.cn
+ * \version 0.1
+ */
+#ifndef CCGL_DB_GDAL_H_
+#define CCGL_DB_GDAL_H_
+#ifdef USE_GDAL
+
+#include <memory>
+
+#include "gdal.h"
+#include "gdal_priv.h"
+#include <ogrsf_frmts.h>
+#include "cpl_string.h"
+#include "ogr_spatialref.h"
+
+//==================== Common: define unified types ====================//
+// Vector dataset: OGRDataSource in GDAL 1.x, GDALDataset in 2.x+
+#if GDAL_VERSION_MAJOR >= 2
+typedef GDALDataset GDALVectorDS;
+#else
+typedef OGRDataSource GDALVectorDS;
+#endif
+
+// Raster dataset: GDALDataset in all versions
+typedef GDALDataset GDALRasterDS;
+
+//==================== Vector: Open / Close ====================//
+
+inline GDALVectorDS* OpenVector(const char* path, int read_only=true) {
+#if GDAL_VERSION_MAJOR >= 2
+    GDALAllRegister();
+    int flags = GDAL_OF_VECTOR | (read_only ? GA_ReadOnly : GA_Update);
+    return static_cast<GDALVectorDS*>(GDALOpenEx(path, flags,
+                                      nullptr, nullptr, nullptr));
+#else
+    OGRRegisterAll();
+    return OGRSFDriverRegistrar::Open(path, read_only ? FALSE : TRUE);
+#endif
+}
+
+inline void CloseVector(GDALVectorDS* ds) {
+    if (!ds) return;
+#if GDAL_VERSION_MAJOR >= 2
+    GDALClose(ds);
+#else
+    OGRDataSource::DestroyDataSource(ds);
+#endif
+}
+
+struct GDALVectorDSDeleter {
+    void operator()(GDALVectorDS* ds) const { CloseVector(ds); }
+};
+typedef std::unique_ptr<GDALVectorDS, GDALVectorDSDeleter> GDALVectorDSHandle;
+
+//==================== Raster: Open / Close / Create ====================//
+
+inline GDALRasterDS* OpenRaster(const char* path, int read_only=true,
+                                const char* const* open_options=nullptr) {
+#if GDAL_VERSION_MAJOR >= 2
+    GDALAllRegister();
+    int flags = GDAL_OF_RASTER | (read_only ? GA_ReadOnly : GA_Update);
+    return static_cast<GDALRasterDS*>(
+        GDALOpenEx(path, flags, nullptr, open_options, nullptr)
+    );
+#else
+    (void)open_options; // avoid unused warnings
+    GDALAllRegister();
+    return static_cast<RasterDs*>(GDALOpen(path, read_only ? GA_ReadOnly : GA_Update));
+#endif
+}
+
+inline GDALRasterDS* CreateRaster(const char* driverName, const char* filename,
+                                  int xsize, int ysize,
+                                  int bands, GDALDataType dtype,
+                                  char** papszOptions=nullptr) {
+    if (!driverName || !driverName[0] || !filename) return nullptr;
+    GDALAllRegister();
+    GDALDriver* drv = GetGDALDriverManager()->GetDriverByName(driverName);
+    if (!drv) return nullptr;
+    return drv->Create(filename, xsize, ysize, bands, dtype, papszOptions);
+}
+
+inline void CloseRaster(GDALRasterDS* ds) {
+    if (!ds) return;
+    GDALClose(ds);
+}
+
+struct GDALRasterDSDeleter {
+    void operator()(GDALRasterDS* ds) const { CloseRaster(ds); }
+};
+typedef std::unique_ptr<GDALRasterDS, GDALRasterDSDeleter> GDALRasterDSHandle;
+#endif // USE_GDAL
+#endif // CCGL_DB_GDAL_H_
